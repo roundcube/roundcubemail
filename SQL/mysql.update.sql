29451cfc61ed7dcd6a55bcdb2358b8f8b9ddeef1
thomascube <thomas@roundcube.net>
2007-09-11 20:51:11 +0000

    Truncate cache table before adding index

--- SQL/mysql.update.sql
+++ SQL/mysql.update.sql
@@ -1,6 +1,8 @@
 -- RoundCube Webmail update script for MySQL databases
 -- Updates from version 0.1-beta and 0.1-beta2
 
+TRUNCATE TABLE `messages`;
+
 ALTER TABLE `messages`
   DROP `body`,
   DROP INDEX `cache_key`,
