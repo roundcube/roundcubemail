6223e957cc1e2e2d27ec09c9432e4a97ba99ea39
thomascube <thomas@roundcube.net>
2007-11-25 15:52:36 +0000

    Fix Postgresql upgrade script (#1484668)

--- SQL/postgres.update.sql
+++ SQL/postgres.update.sql
@@ -5,5 +5,8 @@ ALTER TABLE "messages" DROP body;
 ALTER TABLE "messages" ADD structure TEXT;
 ALTER TABLE "messages" ADD UNIQUE (user_id, cache_key, uid);
 
-ALTER TABLE "identities" ADD html_signature integer DEFAULT 0 NOT NULL;
+ALTER TABLE "identities" ADD html_signature INTEGER;
+ALTER TABLE "identities" ALTER html_signature SET DEFAULT 0;
+UPDATE identities SET html_signature = 0;
+ALTER TABLE "identities" ALTER html_signature SET NOT NULL;
 
