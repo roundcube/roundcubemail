73fef99d2c6463c8569b5114c2e725240b7b52e9
thomascube <thomas@roundcube.net>
2008-03-03 21:42:28 +0000

    Alter README and version strings

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  +-----------------------------------------------------------------------+
  | RoundCube Webmail IMAP Client                                         |
- | Version 0.1-20080302                                                  |
+ | Version 0.1-stable                                                    |
  |                                                                       |
  | Copyright (C) 2005-2007, RoundCube Dev. - Switzerland                 |
  | Licensed under the GNU GPL                                            |
@@ -41,7 +41,7 @@
 */
 
 // application constants
-define('RCMAIL_VERSION', '0.1-trunk');
+define('RCMAIL_VERSION', '0.1');
 define('RCMAIL_CHARSET', 'UTF-8');
 define('JS_OBJECT_NAME', 'rcmail');
 
