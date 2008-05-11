a6f90e1b2bea6602f7056986ee8dbd59afeee90c
alecpl <alec@alec.pl>
2008-05-11 17:07:45 +0000

    -fixed disclaimer

--- index.php
+++ index.php
@@ -1,40 +1,27 @@
 <?php
 /*
- +-----------------------------------------------------------------------+
- | RoundCube Webmail IMAP Client                                         |
- | Version 0.1-20080506                                                  |
- |                                                                       |
- | Copyright (C) 2005-2008, RoundCube Dev. - Switzerland                 |
- | Licensed under the GNU GPL                                            |
- |                                                                       |
- | Redistribution and use in source and binary forms, with or without    |
- | modification, are permitted provided that the following conditions    |
- | are met:                                                              |
- |                                                                       |
- | o Redistributions of source code must retain the above copyright      |
- |   notice, this list of conditions and the following disclaimer.       |
- | o Redistributions in binary form must reproduce the above copyright   |
- |   notice, this list of conditions and the following disclaimer in the |
- |   documentation and/or other materials provided with the distribution.|
- | o The names of the authors may not be used to endorse or promote      |
- |   products derived from this software without specific prior written  |
- |   permission.                                                         |
- |                                                                       |
- | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
- | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
- | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
- | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
- | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
- | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
- | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
- | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
- | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
- | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
- | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
- |                                                                       |
- +-----------------------------------------------------------------------+
- | Author: Thomas Bruederli <roundcube@gmail.com>                        |
- +-----------------------------------------------------------------------+
+ +-------------------------------------------------------------------------+
+ | RoundCube Webmail IMAP Client                                           |
+ | Version 0.1-20080506                                                    |
+ |                                                                         |
+ | Copyright (C) 2005-2008, RoundCube Dev. - Switzerland                   |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or modify    |
+ | it under the terms of the GNU General Public License version 2          |
+ | as published by the Free Software Foundation.                           |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ |                                                                         |
+ | You should have received a copy of the GNU General Public License along |
+ | with this program; if not, write to the Free Software Foundation, Inc., |
+ | 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.             |
+ |                                                                         |
+ +-------------------------------------------------------------------------+
+ | Author: Thomas Bruederli <roundcube@gmail.com>                          |
+ +-------------------------------------------------------------------------+
 
  $Id$
 
