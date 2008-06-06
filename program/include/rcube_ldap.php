70a05665624822dbba5a6417a782709b7067d190
alecpl <alec@alec.pl>
2008-06-06 09:42:31 +0000

    - just boolean check

--- program/include/rcube_ldap.php
+++ program/include/rcube_ldap.php
@@ -94,7 +94,7 @@ class rcube_ldap
     {
       if ($lc = @ldap_connect($host, $this->prop['port']))
       {
-	if ($this->prop['use_tls']===true || strtoupper($this->prop['use_tls'])==='TRUE')
+	if ($this->prop['use_tls']===true)
 	  if (!ldap_start_tls($lc))
 	    continue;
 
