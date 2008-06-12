ac6e28409fd69ad57865c0fa184f96e0df82b068
alecpl <alec@alec.pl>
2008-06-12 12:45:14 +0000

    - http://pear.php.net/bugs/bug.php?id=14124

--- program/lib/MDB2.php
+++ program/lib/MDB2.php
@@ -3008,9 +3008,9 @@ class MDB2_Driver_Common extends PEAR
      */
     function _skipDelimitedStrings($query, $position, $p_position)
     {
-        $ignores = $this->sql_comments;
-        $ignores[] = $this->string_quoting;
+        $ignores = $this->string_quoting;
         $ignores[] = $this->identifier_quoting;
+        $ignores[] = $this->sql_comments;
         
         foreach ($ignores as $ignore) {
             if (!empty($ignore['start'])) {
