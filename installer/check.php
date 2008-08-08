d4f264827af7816eb8cf242f925a9594454eea1f
alecpl <alec@alec.pl>
2008-08-08 08:41:27 +0000

    #1485155: added zend.ze1_compatibility_mode check in installer and option disabled in .htaccess

--- installer/check.php
+++ installer/check.php
@@ -15,7 +15,8 @@ $supported_dbs = array('MySQL' => 'mysql', 'MySQLi' => 'mysqli',
     'PostgreSQL' => 'pgsql', 'SQLite (v2)' => 'sqlite');
 
 $ini_checks = array('file_uploads' => 1, 'session.auto_start' => 0,
-    'magic_quotes_gpc' => 0, 'magic_quotes_sybase' => 0);
+    'magic_quotes_gpc' => 0, 'magic_quotes_sybase' => 0, 
+    'zend.ze1_compatibility_mode' => 0);
 
 $source_urls = array(
     'Sockets' => 'http://www.php.net/manual/en/ref.sockets.php',
