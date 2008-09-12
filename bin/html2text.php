9770fe5cfbb6c61b8ce52c49fc481a9c8c3b4060
alecpl <alec@alec.pl>
2008-09-12 11:22:43 +0000

    #1485338: decode html entities before html2text conversion

--- bin/html2text.php
+++ bin/html2text.php
@@ -3,9 +3,9 @@
 define('INSTALL_PATH', realpath('./../') . '/');
 require INSTALL_PATH.'program/include/iniset.php';
 
-$converter = new html2text($HTTP_RAW_POST_DATA);
+$converter = new html2text(html_entity_decode($HTTP_RAW_POST_DATA, ENT_COMPAT, 'UTF-8'));
 
 header('Content-Type: text/plain; charset=UTF-8');
-print html_entity_decode(trim($converter->get_text()), ENT_COMPAT, 'UTF-8');
+print trim($converter->get_text());
 
 ?>
