bd0c2b0a341c5440c66f9b0e9d2ad2b869401f03
alecpl <alec@alec.pl>
2008-11-21 09:48:50 +0000

    - add mbstring.func_overload check

--- installer/check.php
+++ installer/check.php
@@ -15,7 +15,7 @@ $supported_dbs = array('MySQL' => 'mysql', 'MySQLi' => 'mysqli',
     'PostgreSQL' => 'pgsql', 'SQLite (v2)' => 'sqlite');
 
 $ini_checks = array('file_uploads' => 1, 'session.auto_start' => 0,
-    'zend.ze1_compatibility_mode' => 0);
+    'zend.ze1_compatibility_mode' => 0, 'mbstring.func_overload' => 0);
 
 $source_urls = array(
     'Sockets' => 'http://www.php.net/manual/en/ref.sockets.php',
