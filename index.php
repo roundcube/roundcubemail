a0630561cffdb366385a2c19d0ad167ee575e61f
thomascube <thomas@roundcube.net>
2009-09-04 10:58:29 +0000

    Removed development stuff and updated versions

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  +-------------------------------------------------------------------------+
  | RoundCube Webmail IMAP Client                                           |
- | Version 0.3-20090814                                                    |
+ | Version 0.3-stable                                                      |
  |                                                                         |
  | Copyright (C) 2005-2009, RoundCube Dev. - Switzerland                   |
  |                                                                         |
