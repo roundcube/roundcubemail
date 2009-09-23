cb9fc852cf39d91dfa1dc915073914fd153e774a
alecpl <alec@alec.pl>
2009-09-23 17:25:05 +0000

    - Fix regular expression for e-mail address (#1486152)

--- program/include/rcube_string_replacer.php
+++ program/include/rcube_string_replacer.php
@@ -39,7 +39,7 @@ class rcube_string_replacer
     $url_chars_within = '\?\.~,!';
 
     $this->link_pattern = "/([\w]+:\/\/|\Wwww\.)([a-z0-9\-\.]+[a-z]{2,4}([$url_chars$url_chars_within]*[$url_chars])?)/i";
-    $this->mailto_pattern = "/([a-z0-9][a-z0-9\-\.\+\_]*@[a-z0-9]([a-z0-9\-][.]?)*[a-z0-9]\\.[a-z]{2,5})/i";
+    $this->mailto_pattern = "/([a-z0-9][a-z0-9\-\.\+\_]*@([a-z0-9]([-a-z0-9]*[a-z0-9])?\\.)+[a-z]{2,5})/i";
   }
 
   /**
