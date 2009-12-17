9273c8379098172fb0e3c70ca457cedf71c479b5
alecpl <alec@alec.pl>
2009-12-17 07:53:52 +0000

    - don't use TRUNCATE on sqlite (#1486374)

--- SQL/sqlite.update.sql
+++ SQL/sqlite.update.sql
@@ -37,7 +37,7 @@ CREATE INDEX ix_cache_created ON cache (created);
 
 -- Updates from version 0.3-stable
 
-TRUNCATE messages;
+DELETE FROM messages;
 DROP INDEX ix_messages_user_cache_uid;
 CREATE UNIQUE INDEX ix_messages_user_cache_uid ON messages (user_id,cache_key,uid);
 CREATE INDEX ix_messages_index ON messages (user_id,cache_key,idx);
