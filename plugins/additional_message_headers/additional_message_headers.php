c33cda1f9163deb95beefa5c8f27324644eaac76
alecpl <alec@alec.pl>
2010-03-19 18:58:59 +0000

    - describe config

--- plugins/additional_message_headers/additional_message_headers.php
+++ plugins/additional_message_headers/additional_message_headers.php
@@ -3,13 +3,15 @@
 /**
  * Additional Message Headers
  *
- * Very simple plugin which will add additional headers to or remove them from outgoing messages.
+ * Very simple plugin which will add additional headers
+ * to or remove them from outgoing messages.
+ *
+ * Enable the plugin in config/main.inc.php and add your desired headers:
+ * $rcmail_config['additional_message_headers'] = array('User-Agent');
  *
  * @version @package_version@
  * @author Ziba Scott
  * @website http://roundcube.net
- *
- * See config.inc.php.dist
  */
 class additional_message_headers extends rcube_plugin
 {
