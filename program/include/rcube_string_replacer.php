d21a05b48117ce7dbef4ad7997ed15c26bccf481
alecpl <alec@alec.pl>
2010-10-27 06:25:13 +0000

    - Fix handling of URLs with semicolon (;) character (#1487088)

--- program/include/rcube_string_replacer.php
+++ program/include/rcube_string_replacer.php
@@ -37,13 +37,13 @@ class rcube_string_replacer
   {
     // Simplified domain expression for UTF8 characters handling
     $utf_domain = '[^?&@"\'\\/()\s\r\t\n]+\\.[a-z]{2,5}';
-    $url = '[a-z0-9%=#+?.:&\\/_~-]+';
+    $url = '[a-z0-9%=#@+?.:;&\\/_~-]+';
 
     $this->link_pattern = "/([\w]+:\/\/|\Wwww\.)($utf_domain($url)?)/i";
     $this->mailto_pattern = "/("
         ."[-\w!\#\$%&\'*+~\/^`|{}=]+(?:\.[-\w!\#\$%&\'*+~\/^`|{}=]+)*"  // local-part
         ."@$utf_domain"                                                 // domain-part
-        ."(\?$url)?"                                                     // e.g. ?subject=test...
+        ."(\?$url)?"                                                    // e.g. ?subject=test...
         .")/i";
   }
 
