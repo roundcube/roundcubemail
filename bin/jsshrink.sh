54f1af8cbae8c6a56df69501ff69a045e970f52d
thomascube <thomas@roundcube.net>
2011-09-05 19:59:20 +0000

    Download closure compiler to /tmp instead of current dir

--- bin/jsshrink.sh
+++ bin/jsshrink.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 JS_DIR=`dirname "$0"`/../program/js
+JAR_DIR='/tmp'
 CLOSURE_COMPILER_URL='http://closure-compiler.googlecode.com/files/compiler-latest.zip'
 
 do_shrink() {
 	rm -f "$2"
-	java -jar compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js="$1" --js_output_file="$2"
+	java -jar $JAR_DIR/compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js="$1" --js_output_file="$2"
 }
 
 if [ ! -d "$JS_DIR" ]; then
 	echo "Directory $JS_DIR not found."
 	exit 1
+fi
+
+if [ ! -w "$JAR_DIR" ]; then
+	JAR_DIR=`dirname "$0"`
 fi
 
 if java -version >/dev/null 2>&1; then
@@ -19,16 +24,16 @@ else
 	exit 1
 fi
 
-if [ ! -r "compiler.jar" ]; then
+if [ ! -r "$JAR_DIR/compiler.jar" ]; then
 	if which wget >/dev/null 2>&1 && which unzip >/dev/null 2>&1; then
 		wget "$CLOSURE_COMPILER_URL" -O "/tmp/$$.zip"
 	elif which curl >/dev/null 2>&1 && which unzip >/dev/null 2>&1; then
 		curl "$CLOSURE_COMPILER_URL" -o "/tmp/$$.zip"
 	else
-		echo "Please download $CLOSURE_COMPILER_URL and extract compiler.jar to this directory."
+		echo "Please download $CLOSURE_COMPILER_URL and extract compiler.jar to $JAR_DIR/."
 		exit 1
 	fi
-	unzip "/tmp/$$.zip" "compiler.jar"
+	(cd $JAR_DIR && unzip "/tmp/$$.zip" "compiler.jar")
 	rm -f "/tmp/$$.zip"
 fi
 
