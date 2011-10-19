831fde154dc88d7e9c0912ac382192094b51f348
thomascube <thomas@roundcube.net>
2011-10-19 15:41:17 +0000

    Set version strings to 0.7-beta1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  +-------------------------------------------------------------------------+
  | Roundcube Webmail IMAP Client                                           |
- | Version 0.7-svn                                                         |
+ | Version 0.7-beta1                                                        |
  |                                                                         |
  | Copyright (C) 2005-2011, The Roundcube Dev Team                         |
  |                                                                         |
