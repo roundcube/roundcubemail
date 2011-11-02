fda58b12a84015a0239bc12abae2e65fdda4391f
till <till@php.net>
2011-11-02 15:09:23 +0000

    remove 3.4.2

--- program/js/tiny_mce/plugins/paste/langs/eu_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('eu.paste_dlg',{
-text_title:"Erabili CTRL+V testua lehioan itsasteko.",
-text_linebreaks:"Mantendu lerro-jauziak",
-word_title:"Erabili CTRL+V testua lehioan itsasteko.."
-});
