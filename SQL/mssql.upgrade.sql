6237c947583f96df072b535d9b3c6fd7a79e2921
Aleksander Machniak <alec@alec.pl>
2012-06-08 13:22:16 +0200

    Fix operator incompat. in some queries on contacts table using MS SQL Server (#1488505)

--- SQL/mssql.upgrade.sql
+++ SQL/mssql.upgrade.sql
@@ -249,3 +249,12 @@ GO
 ALTER TABLE [dbo].[contacts] ALTER COLUMN [email] [text] COLLATE Latin1_General_CI_AI NOT NULL
 GO
 
+-- Updates from version 0.8-rc
+
+ALTER TABLE [dbo].[contacts] DROP CONSTRAINT [DF_contacts_email]
+GO
+ALTER TABLE [dbo].[contacts] ALTER COLUMN [email] [varchar] (8000) COLLATE Latin1_General_CI_AI NOT NULL
+GO
+ALTER TABLE [dbo].[contacts] ADD CONSTRAINT [DF_contacts_email] DEFAULT ('') FOR [email]
+GO
+
