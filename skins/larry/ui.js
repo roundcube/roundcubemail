af32a2f5ece250427aa753b236e48784ffa07aba
Thomas Bruederli <thomas@roundcube.net>
2012-07-06 12:53:07 +0200

    Improve iframe form buttons display: fix iframe heights and make footer buttons float if scrolling is active

--- skins/larry/ui.js
+++ skins/larry/ui.js
@@ -220,6 +220,7 @@ function rcube_mail_ui()
 
     // don't use $(window).resize() due to some unwanted side-effects
     window.onresize = resize;
+    resize();
   }
 
   /**
@@ -257,6 +258,20 @@ function rcube_mail_ui()
     if (rcmail.env.task == 'mail' && rcmail.env.action == 'compose') {
       layout_composeview();
     }
+
+    // make iframe footer buttons float if scrolling is active
+    $('body.iframe .footerleft').each(function(){
+      var footer = $(this),
+        body = $(document.body),
+        floating = footer.hasClass('floating'),
+        overflow = body.outerHeight(true) > $(window).height();
+      if (overflow != floating) {
+        var action = overflow ? 'addClass' : 'removeClass';
+        footer[action]('floating');
+        body[action]('floatingbuttons');
+      }
+    })
+
   }
 
   /**
