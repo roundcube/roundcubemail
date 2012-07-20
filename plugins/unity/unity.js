7531bc5067997493d8ac36dfb535ab0b32f0d1a1
Jean-Tiare Le Bigot <jtlebi@gmail.com>
2012-07-20 04:04:44 +0200

    scoped local variables

--- plugins/unity/unity.js
+++ plugins/unity/unity.js
@@ -7,10 +7,10 @@ function unityReady() {
 rcmail.addEventListener('init', function(evt) {
   Unity = external.getUnityObject(1.0);
   
-  title = document.title.split(' ::')[0];
-  url = window.location.href;
-  end = url.lastIndexOf('/');
-  icon = url.slice(0, end)+'/plugins/unity/media/logo.png';
+  var title = document.title.split(' ::')[0];
+  var url = window.location.href;
+  var end = url.lastIndexOf('/');
+  var icon = url.slice(0, end)+'/plugins/unity/media/logo.png';
  
   Unity.init({name: title,
               iconUrl: icon,
