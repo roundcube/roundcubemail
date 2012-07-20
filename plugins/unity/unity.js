9461af709b25c8d561d0665603b41a86726a5d98
Jean-Tiare Le Bigot <jtlebi@gmail.com>
2012-07-20 14:54:04 +0200

    wraped code in a closure, added [inbox, compose, contacts, settings] commands to the Unity launcher

--- plugins/unity/unity.js
+++ plugins/unity/unity.js
@@ -1,19 +1,48 @@
-var Unity = null;
+(function() {
+  /* Variables */
+  var Unity = null;
 
-function unityReady() {
-  // Integrate with Unity!
-}
-
-rcmail.addEventListener('init', function(evt) {
-  Unity = external.getUnityObject(1.0);
-  
   var title = document.title.split(' ::')[0];
   var url = window.location.href;
   var end = url.lastIndexOf('/');
   var icon = url.slice(0, end)+'/plugins/unity/media/logo.png';
- 
-  Unity.init({name: title,
-              iconUrl: icon,
-              onInit: unityReady});
-});
+  
+  /* Actions callbacks */
+  function goto_inbox() {
+    if(rcmail.task == 'mail') {
+      rcmail.command('list', 'INBOX');
+    } else {
+      rcmail.env.mailbox = 'INBOX'; //force INBOX folder
+      rcmail.command('switch-task', 'mail');
+    }
+  }
 
+  function goto_compose() {
+    rcmail.command('compose', '');
+  }
+
+  function goto_contacts() {
+    rcmail.command('switch-task', 'addressbook');
+  }
+
+  function goto_settings() {
+    rcmail.command('switch-task', 'settings');
+  }
+
+  /* Main Setup */
+  function unityReady() {
+    // Launcher menu
+    Unity.Launcher.addAction("Inbox", goto_inbox);
+    Unity.Launcher.addAction("New mail", goto_compose);
+    Unity.Launcher.addAction("Contacts", goto_contacts);
+    Unity.Launcher.addAction("Settings", goto_settings);
+  }
+
+  rcmail.addEventListener('init', function(evt) {
+    Unity = external.getUnityObject(1.0);
+ 
+    Unity.init({name: title,
+                iconUrl: icon,
+                onInit: unityReady});
+  });
+})();
