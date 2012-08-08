2bbc3da52aee81e920e46778d68278bd31f7bb6b
Aleksander Machniak <alec@alec.pl>
2012-08-08 08:44:46 +0200

    - Check request tokens also in devel_mode

--- index.php
+++ index.php
@@ -223,7 +223,7 @@ else {
 
   // check client X-header to verify request origin
   if ($OUTPUT->ajax_call) {
-    if (rcube_utils::request_header('X-Roundcube-Request') != $RCMAIL->get_request_token() && !$RCMAIL->config->get('devel_mode')) {
+    if (rcube_utils::request_header('X-Roundcube-Request') != $RCMAIL->get_request_token()) {
       header('HTTP/1.1 403 Forbidden');
       die("Invalid Request");
     }
