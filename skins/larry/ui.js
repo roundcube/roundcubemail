389fa1d59f99806d73223e8379f9add740bfec82
Aleksander Machniak <alec@alec.pl>
2012-08-08 09:29:59 +0200

    Add splitter for preferences sections list

--- skins/larry/ui.js
+++ skins/larry/ui.js
@@ -145,6 +145,10 @@ function rcube_mail_ui()
         new rcube_splitter({ id:'identviewsplitter', p1:'#identitieslist', p2:'#identity-details',
           orientation:'v', relative:true, start:266, min:180, size:12 }).init();
       }
+      else if (rcmail.env.action == 'preferences') {
+        new rcube_splitter({ id:'prefviewsplitter', p1:'#sectionslist', p2:'#preferences-box',
+          orientation:'v', relative:true, start:266, min:180, size:12 }).init();
+      }
     }
     /***  addressbook task  ***/
     else if (rcmail.env.task == 'addressbook') {
