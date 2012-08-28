dfef514dccc2a3be3fa068ecfa34038fc8504fab
David Warden <dfwarden@gmail.com>
2012-08-28 11:12:04 -0400

    Some cleanup and enabled upload file size check, filedrop upload progress for APC and lighttpd

--- program/js/app.js
+++ program/js/app.js
@@ -6172,7 +6172,9 @@ function rcube_webmail()
   this.async_upload_form = function(form, action, onload)
   {
     var ts = new Date().getTime(),
-      frame_name = 'rcmupload'+ts;
+      frame_name = 'rcmupload'+ts,
+      url_params = { _id:this.env.compose_id||'', _uploadid:ts },
+      lighttpd_progressid = '';
 
     // upload progress support
     if (this.env.upload_progress_name) {
@@ -6186,12 +6188,13 @@ function rcube_webmail()
 
       // lighttpd upload progress support, else APC
       if (this.env.upload_progress_name == 'X-Progress-ID') {
-        var upload_uuid = '';
         for (i = 0; i < 32; i++) {
-          upload_uuid += Math.floor(Math.random() * 16).toString(16);
-        };
-        frame_name = 'rcmupload'+upload_uuid;
-        field.val(upload_uuid);
+          lighttpd_progressid += Math.floor(Math.random() * 16).toString(16);
+        }
+        url_params._uploadid = lighttpd_progressid;
+        $.extend(url_params, { 'X-Progress-ID':lighttpd_progressid });
+        frame_name = 'rcmupload'+lighttpd_progressid;
+        field.val(lighttpd_progressid);
       }else{
         field.val(ts);
       }
@@ -6218,7 +6221,7 @@ function rcube_webmail()
 
     $(form).attr({
         target: frame_name,
-        action: this.url(action, (upload_uuid) ? { _id:this.env.compose_id||'', _uploadid:upload_uuid||ts, 'X-Progress-ID':upload_uuid} : { _id:this.env.compose_id||'', _uploadid:ts }),
+        action: this.url(action, url_params),
         method: 'POST'})
       .attr(form.encoding ? 'encoding' : 'enctype', 'multipart/form-data')
       .submit();
@@ -6262,7 +6265,18 @@ function rcube_webmail()
     var submit_data = function() {
       var multiple = files.length > 1,
         ts = new Date().getTime(),
+        lighttpd_progressid = ''
+        upload_headers = { 'X-Roundcube-Request' : ref.env.request_token },
         content = '<span>' + (multiple ? ref.get_label('uploadingmany') : files[0].name) + '</span>';
+
+      // lighttpd upload progress support
+      if (ref.env.upload_progress_name && ref.env.upload_progress_name == 'X-Progress-ID') {
+        for (i = 0; i < 32; i++) {
+          lighttpd_progressid += Math.floor(Math.random() * 16).toString(16);
+        }
+        ts = lighttpd_progressid;
+        $.extend(upload_headers, { 'X-Progress-ID':lighttpd_progressid });
+      }
 
       // add to attachments list
       if (!ref.add2attachment_list(ts, { name:'', html:content, classname:'uploading', complete:false }))
@@ -6278,19 +6292,32 @@ function rcube_webmail()
         contentType: formdata ? false : 'multipart/form-data; boundary=' + boundary,
         processData: false,
         data: formdata || multipart,
-        headers: {'X-Roundcube-Request': ref.env.request_token},
+        headers: upload_headers,
         beforeSend: function(xhr, s) { if (!formdata && xhr.sendAsBinary) xhr.send = xhr.sendAsBinary; },
         success: function(data){ ref.http_response(data); },
         error: function(o, status, err) { ref.http_error(o, status, err, null, 'attachment'); }
       });
+
+      // upload progress support
+      if (ref.env.upload_progress_time) {
+        ref.upload_progress_start('upload', ts);
+      }
     };
 
     // get contents of all dropped files
-    var last = this.env.filedrop.single ? 0 : files.length - 1;
+    var last = this.env.filedrop.single ? 0 : files.length - 1,
+      size = 0;
     for (var j=0, i=0, f; j <= last && (f = files[i]); i++) {
       if (!f.name) f.name = f.fileName;
       if (!f.size) f.size = f.fileSize;
       if (!f.type) f.type = 'application/octet-stream';
+
+      // check cumulative file size
+      size += f.size;
+      if (size && size > ref.env.max_filesize) {
+        this.display_message(this.env.filesizeerror, 'error');
+        return;
+      }
 
       // file name contains non-ASCII characters, do UTF8-binary string conversion.
       if (!formdata && /[^\x20-\x7E]/.test(f.name))
