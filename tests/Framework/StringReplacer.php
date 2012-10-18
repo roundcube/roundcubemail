22c67d0ec28f4c9488d26aa35151392a18c74c45
Aleksander Machniak <alec@alec.pl>
2012-10-18 10:49:58 +0200

    Fix handling of URLs with asterisk characters (#1488759)

--- tests/Framework/StringReplacer.php
+++ tests/Framework/StringReplacer.php
@@ -17,4 +17,28 @@ class Framework_StringReplacer extends PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('rcube_string_replacer', $sr, "Class constructor");
     }
+
+    /**
+     * Data for test_replace()
+     */
+    function data_replace()
+    {
+        return array(
+            array('http://domain.tld/path*path2', '<a href="http://domain.tld/path*path2" target="_blank">http://domain.tld/path*path2</a>'),
+            array('www.domain.tld', '<a href="http://www.domain.tld" target="_blank">www.domain.tld</a>'),
+            array('WWW.DOMAIN.TLD', '<a href="http://WWW.DOMAIN.TLD" target="_blank">WWW.DOMAIN.TLD</a>'),
+        );
+    }
+
+    /**
+     * @dataProvider data_replace
+     */
+    function test_replace($input, $output)
+    {
+        $replacer = new rcube_string_replacer;
+        $result = $replacer->replace($input);
+        $result = $replacer->resolve($result);
+
+        $this->assertEquals($output, $result);
+    }
 }
