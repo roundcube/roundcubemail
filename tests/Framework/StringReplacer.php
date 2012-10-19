c96d3f57ef6a13e3e61338f57a473b4817547da8
Thomas Bruederli <thomas@roundcube.net>
2012-10-19 15:13:14 +0200

    Match http://localhost/xxx as link

--- tests/Framework/StringReplacer.php
+++ tests/Framework/StringReplacer.php
@@ -25,6 +25,8 @@ class Framework_StringReplacer extends PHPUnit_Framework_TestCase
     {
         return array(
             array('http://domain.tld/path*path2', '<a href="http://domain.tld/path*path2" target="_blank">http://domain.tld/path*path2</a>'),
+            array("Click this link:\nhttps://mail.xn--brderli-o2a.ch/rc/ EOF", "Click this link:\n<a href=\"https://mail.xn--brderli-o2a.ch/rc/\" target=\"_blank\">https://mail.xn--brderli-o2a.ch/rc/</a> EOF"),
+            array('Start http://localhost/?foo End', 'Start <a href="http://localhost/?foo" target="_blank">http://localhost/?foo</a> End'),
             array('www.domain.tld', '<a href="http://www.domain.tld" target="_blank">www.domain.tld</a>'),
             array('WWW.DOMAIN.TLD', '<a href="http://WWW.DOMAIN.TLD" target="_blank">WWW.DOMAIN.TLD</a>'),
         );
