0679b22150dfded4383d0729940a888fcc7a1b99
Aleksander Machniak <alec@alec.pl>
2012-11-15 13:55:42 +0100

    Improved rcube_charset::detect() with BOM checks from rcube_vcard::detect_encoding() - removed code duplication

--- program/include/rcube_vcard.php
+++ program/include/rcube_vcard.php
@@ -784,42 +784,9 @@ class rcube_vcard
    */
   private static function detect_encoding($string)
   {
-    if (substr($string, 0, 4) == "\0\0\xFE\xFF") return 'UTF-32BE';  // Big Endian
-    if (substr($string, 0, 4) == "\xFF\xFE\0\0") return 'UTF-32LE';  // Little Endian
-    if (substr($string, 0, 2) == "\xFE\xFF")     return 'UTF-16BE';  // Big Endian
-    if (substr($string, 0, 2) == "\xFF\xFE")     return 'UTF-16LE';  // Little Endian
-    if (substr($string, 0, 3) == "\xEF\xBB\xBF") return 'UTF-8';
+    $fallback = rcube::get_instance()->config->get('default_charset', 'ISO-8859-1'); // fallback to Latin-1
 
-    // heuristics
-    if ($string[0] == "\0" && $string[1] == "\0" && $string[2] == "\0" && $string[3] != "\0") return 'UTF-32BE';
-    if ($string[0] != "\0" && $string[1] == "\0" && $string[2] == "\0" && $string[3] == "\0") return 'UTF-32LE';
-    if ($string[0] == "\0" && $string[1] != "\0" && $string[2] == "\0" && $string[3] != "\0") return 'UTF-16BE';
-    if ($string[0] != "\0" && $string[1] == "\0" && $string[2] != "\0" && $string[3] == "\0") return 'UTF-16LE';
-
-    // use mb_detect_encoding()
-    $encodings = array('UTF-8', 'ISO-8859-1', 'ISO-8859-2', 'ISO-8859-3',
-      'ISO-8859-4', 'ISO-8859-5', 'ISO-8859-6', 'ISO-8859-7', 'ISO-8859-8', 'ISO-8859-9',
-      'ISO-8859-10', 'ISO-8859-13', 'ISO-8859-14', 'ISO-8859-15', 'ISO-8859-16',
-      'WINDOWS-1252', 'WINDOWS-1251', 'BIG5', 'GB2312');
-
-    if (function_exists('mb_detect_encoding') && ($enc = mb_detect_encoding($string, $encodings)))
-      return $enc;
-
-    // No match, check for UTF-8
-    // from http://w3.org/International/questions/qa-forms-utf-8.html
-    if (preg_match('/\A(
-        [\x09\x0A\x0D\x20-\x7E]
-        | [\xC2-\xDF][\x80-\xBF]
-        | \xE0[\xA0-\xBF][\x80-\xBF]
-        | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}
-        | \xED[\x80-\x9F][\x80-\xBF]
-        | \xF0[\x90-\xBF][\x80-\xBF]{2}
-        | [\xF1-\xF3][\x80-\xBF]{3}
-        | \xF4[\x80-\x8F][\x80-\xBF]{2}
-        )*\z/xs', substr($string, 0, 2048)))
-      return 'UTF-8';
-
-    return rcube::get_instance()->config->get('default_charset', 'ISO-8859-1'); # fallback to Latin-1
+    return rcube_charset::detect($string, $fallback);
   }
 
 }
