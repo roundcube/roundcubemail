roundcube/roundcubemail

commit d30460ad2fc0f78ce44d474fa2c466d660596d27

Let some env variables survive reset(); don't show taskbar and top line in framed error pages (#1488938)

program/include/rcmail_output_html.php
@@ -80,6 +80,8 @@ class rcmail_output_html extends rcmail_output
 
         if (!empty($_REQUEST['_extwin']))
           $this->set_env('extwin', 1);
+        if ($this->framed || !empty($_REQUEST['_framed']))
+          $this->set_env('framed', 1);
 
         // add common javascripts
         $this->add_script('var '.self::JS_OBJECT_NAME.' = new rcube_webmail();', 'head_top');
@@ -308,8 +310,12 @@ class rcmail_output_html extends rcmail_output
      */
     public function reset()
     {
+        $env = array_intersect_key($this->env, array('extwin'=>1, 'framed'=>1));
+
         parent::reset();
-        $this->js_env = array();
+
+        // let some env variables survive
+        $this->env = $this->js_env = $env;
         $this->js_labels = array();
         $this->js_commands = array();
         $this->script_files = array();

skins/larry/includes/header.html
@@ -10,16 +10,16 @@
 	<roundcube:container name="topline-center" id="topline-center" />
 	<div class="topright">
 	<roundcube:container name="topline-right" id="topline-right" />
-	<roundcube:if condition="!env:extwin" />
+	<roundcube:if condition="!env:extwin &amp;&amp; !env:framed" />
 		<span class="username"><roundcube:object name="username" /></span>
 		<roundcube:button command="logout" label="logout" class="button-logout" />
-	<roundcube:else />
+	<roundcube:elseif condition="env:extwin" />
 		<roundcube:button command="close" label="close" class="closelink" />
 	<roundcube:endif />
 	</div>
 </div>
 
-<roundcube:if condition="!env:extwin" />
+<roundcube:if condition="!env:extwin &amp;&amp; !env:framed" />
 <div id="topnav">
 	<div id="taskbar" class="topright">
 	<roundcube:button command="mail" label="mail" class="button-mail" classSel="button-mail button-selected" innerClass="button-inner" />

skins/larry/styles.css
@@ -1276,6 +1276,10 @@ body.iframe {
 	margin: 38px 0 10px 0;
 }
 
+body.iframe.error {
+	background: #ededed;
+}
+
 body.iframe.floatingbuttons {
 	margin-bottom: 40px;
 }

skins/larry/templates/error.html
@@ -4,9 +4,12 @@
 <title><roundcube:object name="pagetitle" /></title>
 <roundcube:include file="/includes/links.html" />
 </head>
-<body>
-
+<roundcube:if condition="!env:framed" />
+<body class="error">
 <roundcube:include file="/includes/header.html" />
+<roundcube:else />
+<body class="error iframe">
+<roundcube:endif />
 
 <div class="uibox centerbox errorbox">
 $__page_content

commit 4d1fe2d230c4194aa99111c5f63dfb33ad35ab83

Fix "rcmail is undefined" error in HTML attachment preview - regression from commit d30460ad2fc0f78ce44d474fa2c466d660596d27, small improvements

program/include/rcmail_html_page.php
@@ -30,7 +30,7 @@ class rcmail_html_page extends rcmail_output_html
 {
     public function write($contents = '')
     {
-        self::reset();
+        self::reset(true);
 
         // load embed.css from skin folder (if exists)
         if ($embed_css = $this->get_skin_file('/embed.css')) {

program/include/rcmail_output_html.php
@@ -307,17 +307,19 @@ class rcmail_output_html extends rcmail_output
 
     /**
      * Delete all stored env variables and commands
+     *
+     * @param bool $all Reset all env variables (including internal)
      */
-    public function reset()
+    public function reset($all = false)
     {
-        $env = array_intersect_key($this->env, array('extwin'=>1, 'framed'=>1));
+        $env = $all ? null : array_intersect_key($this->env, array('extwin'=>1, 'framed'=>1));
 
         parent::reset();
 
         // let some env variables survive
         $this->env = $this->js_env = $env;
-        $this->js_labels = array();
-        $this->js_commands = array();
+        $this->js_labels    = array();
+        $this->js_commands  = array();
         $this->script_files = array();
         $this->scripts      = array();
         $this->header       = '';
@@ -362,7 +364,7 @@ class rcmail_output_html extends rcmail_output
             $this->parse($templ, false);
         }
         else {
-            $this->framed = $templ == 'iframe' ? true : $this->framed;
+            $this->framed = true;
             $this->write();
         }
 
@@ -396,9 +398,11 @@ class rcmail_output_html extends rcmail_output
           $this->set_env('request_token', $this->app->get_request_token());
 
         // write all env variables to client
-        $js = $this->framed ? "if(window.parent) {\n" : '';
-        $js .= $this->get_js_commands() . ($this->framed ? ' }' : '');
-        $this->add_script($js, 'head_top');
+        if ($commands = $this->get_js_commands()) {
+            $js = $this->framed ? "if (window.parent) {\n" : '';
+            $js .= $commands . ($this->framed ? ' }' : '');
+            $this->add_script($js, 'head_top');
+        }
 
         // send clickjacking protection headers
         $iframe = $this->framed || !empty($_REQUEST['_framed']);