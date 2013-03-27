217694ebe579759a575aa42bb755bf5f3670b597
Thomas Bruederli <thomas@roundcube.net>
2013-03-27 17:48:50 +0100

    Bump version for 0.8.6 release

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  +-------------------------------------------------------------------------+
  | Roundcube Webmail IMAP Client                                           |
- | Version 0.8.5                                                           |
+ | Version 0.8.6                                                           |
  |                                                                         |
  | Copyright (C) 2005-2012, The Roundcube Dev Team                         |
  |                                                                         |
