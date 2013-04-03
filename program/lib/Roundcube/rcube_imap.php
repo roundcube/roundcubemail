ea002b6d7637497d9caab39381fbe66985634fac
Aleksander Machniak <alec@alec.pl>
2013-04-03 16:03:57 +0200

    Fix possible header duplicates when using additional headers (#1489033)

--- program/lib/Roundcube/rcube_imap.php
+++ program/lib/Roundcube/rcube_imap.php
@@ -3371,7 +3371,6 @@ class rcube_imap extends rcube_storage
     {
         if (!empty($this->options['fetch_headers'])) {
             $headers = explode(' ', $this->options['fetch_headers']);
-            $headers = array_map('strtoupper', $headers);
         }
         else {
             $headers = array();
@@ -3381,7 +3380,7 @@ class rcube_imap extends rcube_storage
             $headers = array_merge($headers, $this->all_headers);
         }
 
-        return implode(' ', array_unique($headers));
+        return $headers;
     }
 
 
