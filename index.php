a7db8f93ef4824ead7b646fa8543940c4adf5d55
Thomas Bruederli <bruederli@kolabsys.com>
2013-04-15 16:54:36 +0200

    Bump version to 0.9-stable

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  +-------------------------------------------------------------------------+
  | Roundcube Webmail IMAP Client                                           |
- | Version 0.9-rc                                                          |
+ | Version 0.9.0                                                          |
  |                                                                         |
  | Copyright (C) 2005-2013, The Roundcube Dev Team                         |
  |                                                                         |
