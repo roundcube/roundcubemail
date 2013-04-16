1352d8ddb022e972ab9ef0e4e75e1b7b642865df
Aleksander Machniak <alec@alec.pl>
2013-04-16 18:54:13 +0200

    Remove redundant ini checks

--- program/include/iniset.php
+++ program/include/iniset.php
@@ -24,21 +24,6 @@
 define('RCMAIL_VERSION', '1.0-git');
 define('RCMAIL_START', microtime(true));
 
-$config = array(
-    // Some users are not using Installer, so we'll check some
-    // critical PHP settings here. Only these, which doesn't provide
-    // an error/warning in the logs later. See (#1486307).
-    'suhosin.session.encrypt' => 0,
-    'session.auto_start'      => 0,
-    'file_uploads'            => 1,
-);
-foreach ($config as $optname => $optval) {
-    if ($optval != ini_get($optname) && @ini_set($optname, $optval) === false) {
-        die("ERROR: Wrong '$optname' option value and it wasn't possible to set it to required value ($optval).\n"
-            ."Check your PHP configuration (including php_admin_flag).");
-    }
-}
-
 if (!defined('INSTALL_PATH')) {
     define('INSTALL_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
 }
