e34e0fd2fa4806a06d92eef1b281f38242a9868a
Dennis1993 <marvin-wegener@outlook.com>
2013-07-26 13:32:40 +0200

    Update SMTP.php
    remove sensless version_compare(). Min PHP Version is 5.2.1 so is a check of PHP 5.1.0 sensless. So never occurs.

--- program/lib/Net/SMTP.php
+++ program/lib/Net/SMTP.php
@@ -595,8 +595,7 @@ class Net_SMTP
          * extension, are connected to an SMTP server which supports the 
          * STARTTLS extension, and aren't already connected over a secure 
          * (SSL) socket connection. */
-        if ($tls && version_compare(PHP_VERSION, '5.1.0', '>=') &&
-            extension_loaded('openssl') && isset($this->_esmtp['STARTTLS']) &&
+        if ($tls && extension_loaded('openssl') && isset($this->_esmtp['STARTTLS']) &&
             strncasecmp($this->host, 'ssl://', 6) !== 0) {
             /* Start the TLS connection attempt. */
             if (PEAR::isError($result = $this->_put('STARTTLS'))) {
