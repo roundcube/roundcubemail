4fbbd81b9b2230df3b06b773b829f0e3991664f3
Aleksander Machniak <alec@alec.pl>
2013-08-26 10:28:34 +0200

    Fix bugs when invoking contact creation form when read-only addressbook is selected (#1489296)

--- program/include/rcmail.php
+++ program/include/rcmail.php
@@ -257,6 +257,23 @@ class rcmail extends rcube
 
 
   /**
+   * Return identifier of the address book object
+   *
+   * @param rcube_addressbook Addressbook source object
+   *
+   * @return string Source identifier
+   */
+  public function get_address_book_id($object)
+  {
+    foreach ($this->address_books as $index => $book) {
+      if ($book === $object) {
+        return $index;
+      }
+    }
+  }
+
+
+  /**
    * Return address books list
    *
    * @param boolean True if the address book needs to be writeable
