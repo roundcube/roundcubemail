09b8e3b9bdfb0f833a710f170636a9ab5c6d33a1
David Deutsch <skore@valanx.org>
2013-09-01 20:41:04 +0200

    no space after function declaration

--- plugins/archive/archive.js
+++ plugins/archive/archive.js
@@ -24,14 +24,14 @@ function rcmail_archive(prop)
 
 // callback for app-onload event
 if (window.rcmail) {
-    rcmail.addEventListener('init', function (evt)
+    rcmail.addEventListener('init', function(evt)
     {
         // register command (directly enable in message view mode)
         rcmail.register_command('plugin.archive', rcmail_archive, (rcmail.env.uid && rcmail.env.mailbox != rcmail.env.archive_folder));
 
         // add event-listener to message list
         if (rcmail.message_list) {
-            rcmail.message_list.addEventListener('select', function (list) {
+            rcmail.message_list.addEventListener('select', function(list) {
                 rcmail.enable_command('plugin.archive', (list.get_selection().length > 0 && rcmail.env.mailbox != rcmail.env.archive_folder));
             });
         }
@@ -44,7 +44,7 @@ if (window.rcmail) {
         }
 
         // callback for server response
-        rcmail.addEventListener('plugin.move2archive_response', function (result) {
+        rcmail.addEventListener('plugin.move2archive_response', function(result) {
             // refresh list
             if (result.update) rcmail.command('checkmail');
         });
