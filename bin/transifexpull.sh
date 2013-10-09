915bac3ebbf2cdc6dcb6a88680e7f364a81e00a2
Aleksander Machniak <alec@alec.pl>
2013-10-09 19:22:41 +0200

    Do not cleanup en_US files

--- bin/transifexpull.sh
+++ bin/transifexpull.sh
@@ -9,13 +9,16 @@ PWD=`dirname "$0"`
 
 do_clean()
 {
+    # do not cleanup en_US files
+    echo "$1" | grep -v en_US > /dev/null || return
+
     echo "Cleaning $1"
 
     # remove untranslated/empty texts
     perl -pi -e "s/^\\\$labels\[[^]]+\]\s+=\s+'';\n//g" $1
     perl -pi -e "s/^\\\$messages\[[^]]+\]\s+=\s+'';\n//g" $1
     # remove (one-line) comments
-    perl -pi -e "s/^\\/\\/[a-z\s]+//g" $1
+    perl -pi -e "s/^\\/\\/[a-zA-Z0-9\s]+//g" $1
     # remove empty lines (but not in file header)
     perl -ne 'print if ($. < 21 || length($_) > 1)' $1 > $1.tmp
     mv $1.tmp $1
@@ -25,6 +28,8 @@ do_clean()
 for file in $PWD/../program/localization/*/*.inc; do
     do_clean $file
 done
+exit
+
 for file in $PWD/../plugins/*/localization/*.inc; do
     do_clean $file
 done
