6e0da1b82ea8fbc1b482e177e5a36efb52c68245
Aleksander Machniak <alec@alec.pl>
2013-10-09 20:11:33 +0200

    Remove also $labels=array() and $messages=array() lines

--- bin/transifexpull.sh
+++ bin/transifexpull.sh
@@ -3,7 +3,7 @@
 # In 'translator' mode files will contain empty translated texts
 # where translation is not available, we'll remove these later
 
-#tx --debug pull -a --mode translator
+tx --debug pull -a --mode translator
 
 PWD=`dirname "$0"`
 
@@ -15,10 +15,12 @@ do_clean()
     echo "Cleaning $1"
 
     # remove untranslated/empty texts
-    perl -pi -e "s/^\\\$labels\[[^]]+\]\s+=\s+'';\n//g" $1
-    perl -pi -e "s/^\\\$messages\[[^]]+\]\s+=\s+'';\n//g" $1
+    perl -pi -e "s/^\\\$labels\[[^]]+\]\s+=\s+'';\n//" $1
+    perl -pi -e "s/^\\\$messages\[[^]]+\]\s+=\s+'';\n//" $1
+    # remove variable initialization
+    perl -pi -e "s/^\\\$(labels|messages)\s*=\s*array\(\);\n//" $1
     # remove (one-line) comments
-    perl -pi -e "s/^\\/\\/.*//g" $1
+    perl -pi -e "s/^\\/\\/.*//" $1
     # remove empty lines (but not in file header)
     perl -ne 'print if ($. < 21 || length($_) > 1)' $1 > $1.tmp
     mv $1.tmp $1
@@ -28,8 +30,6 @@ do_clean()
 for file in $PWD/../program/localization/*/*.inc; do
     do_clean $file
 done
-exit
-
 for file in $PWD/../plugins/*/localization/*.inc; do
     do_clean $file
 done
