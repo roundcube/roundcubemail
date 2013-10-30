07f161dd4e3aeaa7d2d21b8d03567c3f3af616d0
Aleksander Machniak <alec@alec.pl>
2013-10-30 10:42:23 +0100

    Improved checking if current folder is an archive folder (or subfolder)

--- plugins/archive/archive.js
+++ plugins/archive/archive.js
@@ -8,7 +8,7 @@ function rcmail_archive(prop)
   if (!rcmail.env.uid && (!rcmail.message_list || !rcmail.message_list.get_selection().length))
     return;
 
-  if (rcmail.env.mailbox.indexOf(rcmail.env.archive_folder) != 0) {
+  if (rcmail_is_archive()) {
     if (!rcmail.env.archive_type) {
       // simply move to archive folder (if no partition type is set)
       rcmail.command('move', rcmail.env.archive_folder);
@@ -20,16 +20,29 @@ function rcmail_archive(prop)
   }
 }
 
+function rcmail_is_archive()
+{
+  if (!rcmail.env.archive_folder)
+    return false;
+
+  // check if current folder is an archive folder or one of its children
+  if (rcmail.env.mailbox == rcmail.env.archive_folder
+    || rcmail.env.mailbox.indexOf(rcmail.env.archive_folder + rcmail.env.delimiter) != 0
+  ) {
+    return true;
+  }
+}
+
 // callback for app-onload event
 if (window.rcmail) {
   rcmail.addEventListener('init', function(evt) {
     // register command (directly enable in message view mode)
-    rcmail.register_command('plugin.archive', rcmail_archive, (rcmail.env.uid && rcmail.env.mailbox != rcmail.env.archive_folder));
+    rcmail.register_command('plugin.archive', rcmail_archive, rcmail.env.uid && rcmail_is_archive());
 
     // add event-listener to message list
     if (rcmail.message_list)
       rcmail.message_list.addEventListener('select', function(list) {
-        rcmail.enable_command('plugin.archive', (list.get_selection().length > 0 && rcmail.env.mailbox != rcmail.env.archive_folder));
+        rcmail.enable_command('plugin.archive', list.get_selection().length > 0 && rcmail_is_archive());
       });
 
     // set css style for archive folder
