8328d0643eaadd30fb969f51cf51a96f622a459d
Aleksander Machniak <alec@alec.pl>
2013-11-18 19:30:58 +0100

    Code improvements and some unification with browser detection code in javascript

--- program/lib/Roundcube/rcube_browser.php
+++ program/lib/Roundcube/rcube_browser.php
@@ -28,37 +28,24 @@ class rcube_browser
     {
         $HTTP_USER_AGENT = strtolower($_SERVER['HTTP_USER_AGENT']);
 
-        $this->ver = 0;
-        $this->win = strpos($HTTP_USER_AGENT, 'win') != false;
-        $this->mac = strpos($HTTP_USER_AGENT, 'mac') != false;
+        $this->ver   = 0;
+        $this->win   = strpos($HTTP_USER_AGENT, 'win') != false;
+        $this->mac   = strpos($HTTP_USER_AGENT, 'mac') != false;
         $this->linux = strpos($HTTP_USER_AGENT, 'linux') != false;
         $this->unix  = strpos($HTTP_USER_AGENT, 'unix') != false;
 
-        $this->opera = strpos($HTTP_USER_AGENT, 'opera') !== false;
-        $this->ns4 = strpos($HTTP_USER_AGENT, 'mozilla/4') !== false && strpos($HTTP_USER_AGENT, 'msie') === false;
-        $this->ns  = ($this->ns4 || strpos($HTTP_USER_AGENT, 'netscape') !== false);
-        $this->ie  = !$this->opera && (strpos($HTTP_USER_AGENT, 'compatible; msie') !== false || strpos($HTTP_USER_AGENT, 'trident/') !== false);
-        $this->khtml = strpos($HTTP_USER_AGENT, 'khtml') !== false;
-        $this->mz  = !$this->ie && !$this->khtml && strpos($HTTP_USER_AGENT, 'mozilla/5') !== false;
+        $this->opera  = strpos($HTTP_USER_AGENT, 'opera') !== false;
+        $this->ns     = strpos($HTTP_USER_AGENT, 'netscape') !== false;
         $this->chrome = strpos($HTTP_USER_AGENT, 'chrome') !== false;
-        $this->safari = !$this->chrome && ($this->khtml || strpos($HTTP_USER_AGENT, 'safari') !== false);
+        $this->ie     = !$this->opera && (strpos($HTTP_USER_AGENT, 'compatible; msie') !== false || strpos($HTTP_USER_AGENT, 'trident/') !== false);
+        $this->safari = !$this->chrome && (strpos($HTTP_USER_AGENT, 'safari') !== false || strpos($HTTP_USER_AGENT, 'applewebkit') !== false);
+        $this->mz     = !$this->ie && !$this->safari && !$this->chrome && !$this->ns && strpos($HTTP_USER_AGENT, 'mozilla') !== false;
 
-        if ($this->ns || $this->chrome) {
-            $test = preg_match('/(mozilla|chrome)\/([0-9.]+)/', $HTTP_USER_AGENT, $regs);
-            $this->ver = $test ? (float)$regs[2] : 0;
+        if (preg_match('/(mozilla|chrome|msie|opera|safari|applewebkit|khtml)(\s*|\/)([0-9.]+)/', $HTTP_USER_AGENT, $regs)) {
+            $this->ver = (float) $regs[3];
         }
-        else if ($this->mz) {
-            $test = preg_match('/rv:([0-9.]+)/', $HTTP_USER_AGENT, $regs);
-            $this->ver = $test ? (float)$regs[1] : 0;
-        }
-        else if ($this->ie || $this->opera) {
-            $test = preg_match('/(msie|opera) ([0-9.]+)/', $HTTP_USER_AGENT, $regs);
-            $this->ver = $test ? (float)$regs[2] : 0;
-
-            // IE 11
-            if (!$this->ver && preg_match('/rv:([0-9.]+)/', $HTTP_USER_AGENT, $regs)) {
-                $this->ver = (float) $regs[1];
-            }
+        else if (preg_match('/rv:([0-9.]+)/', $HTTP_USER_AGENT, $regs)) {
+            $this->ver = (float) $regs[1];
         }
 
         if (preg_match('/ ([a-z]{2})-([a-z]{2})/', $HTTP_USER_AGENT, $regs))
@@ -66,10 +53,10 @@ class rcube_browser
         else
             $this->lang =  'en';
 
-        $this->dom = ($this->mz || $this->safari || ($this->ie && $this->ver>=5) || ($this->opera && $this->ver>=7));
+        $this->dom      = $this->mz || $this->safari || ($this->ie && $this->ver>=5) || ($this->opera && $this->ver>=7);
         $this->pngalpha = $this->mz || $this->safari || ($this->ie && $this->ver>=5.5) ||
             ($this->ie && $this->ver>=5 && $this->mac) || ($this->opera && $this->ver>=7) ? true : false;
-        $this->imgdata = !$this->ie;
+        $this->imgdata  = !$this->ie;
     }
 }
 
