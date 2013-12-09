roundcube/roundcubemail

commit e71ceb0c98b33409af5978f0f33c6c95148bd86b

Remove useless height attribute from #quotadisplay element

skins/larry/styles.css
@@ -2014,7 +2014,6 @@ select.decorated option {
 	font-weight: bold;
 	text-shadow: 0px 1px 1px #fff;
 	padding-left: 30px;
-	height: 18px;
 	background: url(images/quota.png) -100px 0 no-repeat;
 }
 

commit edca654ef0e3f2003de587c13f2402a9518972dd

Fix quota image broken in e71ceb0c98b33409af5978f0f33c6c95148bd86b (#1489467)

program/include/rcmail.php
@@ -1630,7 +1630,7 @@ class rcmail extends rcube
 
         $rcmail->output->add_script('rcmail.set_quota('.rcube_output::json_serialize($quota).');', 'docready');
 
-        return html::span($attrib, '');
+        return html::span($attrib, '&nbsp;');
     }
 
 

skins/larry/styles.css
@@ -2019,6 +2019,7 @@ select.decorated option {
 
 #quotadisplay {
 	left: 6px;
+	height: 18px;
 	font-size: 12px;
 	font-weight: bold;
 	text-shadow: 0px 1px 1px #fff;