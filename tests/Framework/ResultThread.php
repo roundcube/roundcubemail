18ff2e4ac68044d94ef9edf403b6638dc9a6b91a
Aleksander Machniak <alec@alec.pl>
2014-01-15 14:19:36 +0100

    Added some tests for IMAP THREAD parser

--- tests/Framework/ResultThread.php
+++ tests/Framework/ResultThread.php
@@ -17,4 +17,28 @@ class Framework_ResultThread extends PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('rcube_result_thread', $object, "Class constructor");
     }
+
+    /**
+     * thread parser test
+     */
+    function test_parse_thread()
+    {
+        $text   = file_get_contents(__DIR__ . '/../src/imap_thread.txt');
+        $object = new rcube_result_thread('INBOX', $text);
+
+        $this->assertSame(false, $object->is_empty(), "Object is empty");
+        $this->assertSame(false, $object->is_error(), "Object is error");
+        $this->assertSame(1721, $object->max(), "Max message UID");
+        $this->assertSame(1, $object->min(), "Min message UID");
+        $this->assertSame(1721, $object->count_messages(), "Messages count");
+        $this->assertSame(1691, $object->exists(1720, true), "Message exists");
+        $this->assertSame(true, $object->exists(1720), "Message exists (bool)");
+        $this->assertSame(1, $object->get_element('FIRST'), "Get first element");
+        $this->assertSame(1719, $object->get_element('LAST'), "Get last element");
+        $this->assertSame(14, (int) $object->get_element(2), "Get specified element");
+
+        $object->filter(array(784));
+        $this->assertSame(118, $object->count_messages(), "Messages filter");
+        $this->assertSame(1, $object->count(), "Messages filter (count)");
+    }
 }
