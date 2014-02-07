8c32f881933c19e57b9148149d1c86eb802be3ff
Aleksander Machniak <alec@alec.pl>
2014-02-07 14:05:46 +0100

    Fixed typo in script output

--- bin/update.sh
+++ bin/update.sh
@@ -147,7 +147,7 @@ if ($RCI->configured) {
   // check file type detection
   if ($RCI->check_mime_detection()) {
     echo "WARNING: File type detection doesn't work properly!\n";
-    echo "Please check the 'mime_magic' config option or the finfo functions of PHP andrun this script again.\n";
+    echo "Please check the 'mime_magic' config option or the finfo functions of PHP and run this script again.\n";
   }
   if ($RCI->check_mime_extensions()) {
     echo "WARNING: Mimetype to file extension mapping doesn't work properly!\n";
