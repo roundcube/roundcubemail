f130f9116e1e9ee9735d8026aa5242fc9daceecb
Aleksander Machniak <alec@alec.pl>
2014-02-07 14:43:51 +0100

    Fix directories check in Installer on Windows (#1489576) Added rcube_utils::is_absolute_path() method

--- installer/test.php
+++ installer/test.php
@@ -91,7 +91,7 @@ if ($RCI->config['log_driver'] != 'syslog')
     $dirs[] = $RCI->config['log_dir'] ? $RCI->config['log_dir'] : 'logs';
 
 foreach ($dirs as $dir) {
-    $dirpath = $dir[0] == '/' ? $dir : INSTALL_PATH . $dir;
+    $dirpath = rcube_utils::is_absolute_path($dir) ? $dir : INSTALL_PATH . $dir;
     if (is_writable(realpath($dirpath))) {
         $RCI->pass($dir);
         $pass = true;
