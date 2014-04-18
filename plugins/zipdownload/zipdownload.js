ca33f32b607966b9d774ae1c6261031056212a33
Thomas Bruederli <thomas@roundcube.net>
2014-04-18 17:26:25 +0200

    Fix submission of multi-folder message selection through hidden form fields

--- plugins/zipdownload/zipdownload.js
+++ plugins/zipdownload/zipdownload.js
@@ -65,7 +65,13 @@ function rcmail_zipdownload(mode)
         post._token = rcmail.env.request_token;
 
         $.each(post, function(k, v) {
-            inputs.push($('<input>').attr({type: 'hidden', name: k, value: v}));
+            if (typeof v == 'object' && v.length > 1) {
+              for (var j=0; j < v.length; j++)
+                  inputs.push($('<input>').attr({type: 'hidden', name: k+'[]', value: v[j]}));
+            }
+            else {
+                inputs.push($('<input>').attr({type: 'hidden', name: k, value: v}));
+            }
         });
 
         if (!form.length)
