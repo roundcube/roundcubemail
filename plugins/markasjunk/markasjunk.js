70a39a7f0d399823fa7bc22859361b877d493c77
brendan braybrook <brendan@tucows.com>
2014-05-08 15:10:33 +0000

    merge header back in

--- plugins/markasjunk/markasjunk.js
+++ plugins/markasjunk/markasjunk.js
@@ -1,4 +1,19 @@
-/* Mark-as-Junk plugin script */
+/**
+ * Mark-as-Junk plugin script
+ *
+ * @licstart  The following is the entire license notice for the
+ * JavaScript code in this file.
+ *
+ * Copyright (c) 2013, The Roundcube Dev Team
+ *
+ * The JavaScript code in this page is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ */
 
 function rcmail_markasjunk(prop)
 {
