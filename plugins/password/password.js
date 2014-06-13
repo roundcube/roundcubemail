abe381e3b0dc85fc0c9eb9fe788733a4ba157fd7
Aleksander Machniak <alec@alec.pl>
2014-06-13 12:44:10 +0200

    Focus first input in password form on page load

--- plugins/password/password.js
+++ plugins/password/password.js
@@ -15,30 +15,33 @@
  * for the JavaScript code in this file.
  */
 
-if (window.rcmail) {
-  rcmail.addEventListener('init', function(evt) {
-
+window.rcmail && rcmail.addEventListener('init', function(evt) {
     // register command handler
-    rcmail.register_command('plugin.password-save', function() { 
-      var input_curpasswd = rcube_find_object('_curpasswd'),
-        input_newpasswd = rcube_find_object('_newpasswd'),
-        input_confpasswd = rcube_find_object('_confpasswd');
+    rcmail.register_command('plugin.password-save', function() {
+        var input_curpasswd = rcube_find_object('_curpasswd'),
+            input_newpasswd = rcube_find_object('_newpasswd'),
+            input_confpasswd = rcube_find_object('_confpasswd');
 
       if (input_curpasswd && input_curpasswd.value == '') {
           alert(rcmail.gettext('nocurpassword', 'password'));
           input_curpasswd.focus();
-      } else if (input_newpasswd && input_newpasswd.value == '') {
+      }
+      else if (input_newpasswd && input_newpasswd.value == '') {
           alert(rcmail.gettext('nopassword', 'password'));
           input_newpasswd.focus();
-      } else if (input_confpasswd && input_confpasswd.value == '') {
+      }
+      else if (input_confpasswd && input_confpasswd.value == '') {
           alert(rcmail.gettext('nopassword', 'password'));
           input_confpasswd.focus();
-      } else if (input_newpasswd && input_confpasswd && input_newpasswd.value != input_confpasswd.value) {
+      }
+      else if (input_newpasswd && input_confpasswd && input_newpasswd.value != input_confpasswd.value) {
           alert(rcmail.gettext('passwordinconsistency', 'password'));
           input_newpasswd.focus();
-      } else {
+      }
+      else {
           rcmail.gui_objects.passform.submit();
       }
     }, true);
-  })
-}
+
+    $('input:not(:hidden):first').focus();
+});
