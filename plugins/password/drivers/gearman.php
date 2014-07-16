9d2550502a68ea3cc495e287ebff04e142a59e2e
Aleksander Machniak <alec@alec.pl>
2014-07-16 15:12:53 +0200

    CS fixes after PR merge

--- plugins/password/drivers/gearman.php
+++ plugins/password/drivers/gearman.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Gearman Password Driver
  *
@@ -11,40 +12,44 @@
 
 class rcube_gearman_password
 {
-  function save($currpass, $newpass)
-  {
-    $user = $_SESSION['username'];
-    $rcmail = rcmail::get_instance();
+    function save($currpass, $newpass)
+    {
+        if (extension_loaded('gearman')) {
+            $rcmail  = rcmail::get_instance();
+            $user    = $_SESSION['username'];
+            $payload = array(
+                'username'    => $user,
+                'oldPassword' => $currpass,
+                'newPassword' => $newpass,
+            );
 
-    if (extension_loaded('gearman')) {
-      $success = false;
-      $gmc= new GearmanClient();
+            $gmc = new GearmanClient();
+            $gmc->addServer($rcmail->config->get('password_gearman_host'));
 
-      $gmc->addServer($rcmail->config->get('password_gearman_host'));
-      $payload = array("username" => $user, "oldPassword" => $currpass, "newPassword" => $newpass);
-      $result = $gmc->doNormal("setPassword", json_encode($payload));
-      $success = json_decode($result);
-      if ($success->result == 1) {
-        return PASSWORD_SUCCESS;
-      } else {
-        rcube::raise_error(array(
-          'code' => 600,
-          'type' => 'php',
-          'file' => __FILE__, 'line' => __LINE__,
-          'message' => "Password plugin: Gearman authentication failed for user $user: $error"
-        ), true, false);
-      }
+            $result  = $gmc->doNormal('setPassword', json_encode($payload));
+            $success = json_decode($result);
+
+            if ($success && $success->result == 1) {
+                return PASSWORD_SUCCESS;
+            }
+            else {
+                rcube::raise_error(array(
+                    'code' => 600,
+                    'type' => 'php',
+                    'file' => __FILE__, 'line' => __LINE__,
+                    'message' => "Password plugin: Gearman authentication failed for user $user: $error"
+                ), true, false);
+            }
+        }
+        else {
+            rcube::raise_error(array(
+                'code' => 600,
+                'type' => 'php',
+                'file' => __FILE__, 'line' => __LINE__,
+                'message' => "Password plugin: PECL Gearman module not loaded"
+            ), true, false);
+        }
+
+        return PASSWORD_ERROR;
     }
-    else {
-      rcube::raise_error(array(
-        'code' => 600,
-        'type' => 'php',
-        'file' => __FILE__, 'line' => __LINE__,
-        'message' => "Password plugin: PECL Gearman module not loaded"
-      ), true, false);
-    }
-
-    return PASSWORD_ERROR;
-  }
 }
-?>
