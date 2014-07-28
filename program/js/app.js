39bd9b8e298b21bbe652b31b940aa772bed7d8b2
Thomas Bruederli <thomas@roundcube.net>
2014-07-28 09:39:37 +0200

    Set 'compose_extwin' env property on every step; accept a list of URL parameters for the 'compose' command

--- program/js/app.js
+++ program/js/app.js
@@ -980,7 +980,7 @@ function rcube_webmail()
         if (this.task == 'mail') {
           url._mbox = this.env.mailbox;
           if (props)
-             url._to = props;
+            url._to = props;
           // also send search request so we can go back to search result after message is sent
           if (this.env.search_request)
             url._search = this.env.search_request;
@@ -1008,8 +1008,12 @@ function rcube_webmail()
             break;
           }
         }
-        else if (props)
+        else if (props && typeof props == 'string') {
           url._to = props;
+        }
+        else if (props && typeof props == 'object') {
+          $.extend(url, props);
+        }
 
         this.open_compose_step(url);
         break;
