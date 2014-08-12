aecdf0644d836660fc06eac91b5528e50f00efa8
Victor Benincasa <vbenincasa@gmail.com>
2014-08-12 01:13:03 -0300

    Remove $config['default_folders'] from config tips
    Remove $config['default_folders'] from config tips as this option was removed on commit dc0b500.
    Related ticket: http://trac.roundcube.net/ticket/1489737

--- config/defaults.inc.php
+++ config/defaults.inc.php
@@ -1028,7 +1028,7 @@ $config['force_7bit'] = false;
 // Defaults of the search field configuration.
 // The array can contain a per-folder list of header fields which should be considered when searching
 // The entry with key '*' stands for all folders which do not have a specific list set.
-// Please note that folder names should to be in sync with $config['default_folders']
+// Please note that folder names should to be in sync with $config['*_mbox'] options
 $config['search_mods'] = null;  // Example: array('*' => array('subject'=>1, 'from'=>1), 'Sent' => array('subject'=>1, 'to'=>1));
 
 // Defaults of the addressbook search field configuration.
