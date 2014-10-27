49af50a14ed7ae5548611d0318fdd9ea8a60b616
Markus Neubauer <neubauer@email-online.org>
2014-10-27 14:18:43 +0100

    Some pw characters do not oberate on some clients
    Some clients (f.e. k9 mail on Android) do not work with mailserver as expected with some characters (f.e. German umlauts)

--- plugins/password/password.php
+++ plugins/password/password.php
@@ -112,6 +112,7 @@ class password extends rcube_plugin
         $confirm = $rcmail->config->get('password_confirm_current');
         $required_length = intval($rcmail->config->get('password_minimum_length'));
         $check_strength  = $rcmail->config->get('password_require_nonalpha');
+        $disallowed_chars = $rcmail->config->get['password_disallowed_chars'];
 
         if (($confirm && !isset($_POST['_curpasswd'])) || !isset($_POST['_newpasswd'])) {
             $rcmail->output->command('display_message', $this->gettext('nopassword'), 'error');
@@ -158,6 +159,9 @@ class password extends rcube_plugin
             // password is the same as the old one, do nothing, return success
             else if ($sespwd == $newpwd && !$rcmail->config->get('password_force_save')) {
                 $rcmail->output->command('display_message', $this->gettext('successfullysaved'), 'confirmation');
+            }
+            else if (!empty($disallowed_chars) && strtr($newpwd, $disallowed_chars, str_repeat("\r",strlen($disallowed_chars))) != $newpwd) {
+                $rcmail->output->command('display_message', $this->gettext('passwordforbidden'), 'error');  
             }
             // try to save the password
             else if (!($res = $this->_save($curpwd, $newpwd))) {
