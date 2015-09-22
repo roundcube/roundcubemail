7d71c489524e8c83992af5f03360b0f3e37d2cb2
Aleksander Machniak <alec@alec.pl>
2015-09-22 11:42:03 +0200

    s/RCMAIL_VERSION/RCUBE_VERSION/

--- program/lib/Roundcube/rcube_spellcheck_googie.php
+++ program/lib/Roundcube/rcube_spellcheck_googie.php
@@ -87,7 +87,7 @@ class rcube_spellcheck_googie extends rcube_spellcheck_engine
         if ($fp = fsockopen($host, $port, $errno, $errstr, 30)) {
             $out = "POST $path HTTP/1.0\r\n";
             $out .= "Host: " . str_replace('ssl://', '', $host) . "\r\n";
-            $out .= "User-Agent: Roundcube Webmail/" . RCMAIL_VERSION . " (Googiespell Wrapper)\r\n";
+            $out .= "User-Agent: Roundcube Webmail/" . RCUBE_VERSION . " (Googiespell Wrapper)\r\n";
             $out .= "Content-Length: " . strlen($gtext) . "\r\n";
             $out .= "Content-Type: text/xml\r\n";
             $out .= "Connection: Close\r\n\r\n";
