2c0a550105f22e17307198bb295f83c210fb7cb5
Thomas Bruederli <thomas@roundcube.net>
2015-12-23 09:48:50 +0100

    Bump version to 1.0.8; update Changelog

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  +-------------------------------------------------------------------------+
  | Roundcube Webmail IMAP Client                                           |
- | Version 1.0.7                                                           |
+ | Version 1.0.8                                                           |
  |                                                                         |
  | Copyright (C) 2005-2014, The Roundcube Dev Team                         |
  |                                                                         |
--- program/include/iniset.php
+++ program/include/iniset.php
@@ -21,7 +21,7 @@
 */
 
 // application constants
-define('RCMAIL_VERSION', '1.0.7');
+define('RCMAIL_VERSION', '1.0.8');
 define('RCMAIL_START', microtime(true));
 
 if (!defined('INSTALL_PATH')) {
