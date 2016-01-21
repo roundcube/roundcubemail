d3b98eb4dcb2b7eb867ae21108e64d0b2769e920
Aleksander Machniak <alec@alec.pl>
2016-01-21 16:27:03 +0100

    Fix (again) security issue in DBMail driver of password plugin [CVE-2015-2181] (#1490643)
    Unify the C program code with the one used by other drivers.
    
    Conflicts:
    	CHANGELOG

--- plugins/password/helpers/chgdbmailusers.c
+++ plugins/password/helpers/chgdbmailusers.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 
 // set the UID this script will run as (root user)
@@ -15,27 +14,10 @@
 
 main(int argc, char *argv[])
 {
-  int cnt,rc,cc;
-  char cmnd[1024];
-
-  strcpy(cmnd, CMD);
-
-  if (argc > 1)
-  {
-    for (cnt = 1; cnt < argc; cnt++)
-    {
-      strcat(cmnd, " ");
-      strcat(cmnd, argv[cnt]);
-    }
-  }
-  else
-  {
-    fprintf(stderr, "__ %s:  failed %d  %d\n", argv[0], rc, cc);
-    return 255;
-  }
+  int rc, cc;
 
   cc = setuid(UID);
-  rc = system(cmnd);
+  rc = execvp(CMD, argv);
 
   if ((rc != 0) || (cc != 0))
   {
