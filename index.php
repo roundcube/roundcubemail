cde7a9eb74b6fd6315885c30c0763e0ee5332499
Thomas Bruederli <thomas@roundcube.net>
2016-04-17 18:39:57 +0200

    Bump version to 1.0.9

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  +-------------------------------------------------------------------------+
  | Roundcube Webmail IMAP Client                                           |
- | Version 1.0.8                                                           |
+ | Version 1.0.9                                                           |
  |                                                                         |
  | Copyright (C) 2005-2014, The Roundcube Dev Team                         |
  |                                                                         |
