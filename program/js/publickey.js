fe1e2b27aa5acab7168940be84dfbf4f9514910b
Thomas Bruederli <thomas@roundcube.net>
2016-05-29 13:07:03 +0200

    Add licence header to cloned PublicKey.js

--- program/js/publickey.js
+++ program/js/publickey.js
@@ -1,5 +1,36 @@
 "use strict";
-
+/**
+ * PublicKey.js
+ * https://github.com/diafygi/publickeyjs
+ *
+ * A simple javascript library that allows you to search for PGP
+ * public keys on SKS keyservers and Keybase. The API and results
+ * are similar to specifications described in HTTP Keyserver Protocol.
+ *
+ * @licstart  The following is the entire license notice for the
+ * JavaScript code in this file.
+ *
+ * Copyright (c) 2015 Daniel Roesler
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ * @author Daniel Roesler (diafygi)
+ */
 (function(context){
     /*
         Default keyservers (HTTPS and CORS enabled)
