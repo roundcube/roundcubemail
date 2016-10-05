41d72ca7dac90f4372b912228a9e3645c9afe703
Aleksander Machniak <alec@alec.pl>
2016-10-05 17:36:33 +0200

    Fix regression where creation of default folders wasn't functioning without prefix (#5460)

--- program/lib/Roundcube/rcube_imap.php
+++ program/lib/Roundcube/rcube_imap.php
@@ -453,6 +453,11 @@ class rcube_imap extends rcube_storage
         $ns = $this->namespace;
 
         if ($name) {
+            // an alias for BC
+            if ($name == 'prefix') {
+                $name = 'prefix_in';
+            }
+
             return isset($ns[$name]) ? $ns[$name] : null;
         }
 
