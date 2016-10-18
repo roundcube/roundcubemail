39690c8a78163dac9bc43a555b121acc18b221d8
Aleksander Machniak <alec@alec.pl>
2016-10-18 08:25:47 +0200

    Fix "DB Write" test (#5479)

--- installer/test.php
+++ installer/test.php
@@ -179,7 +179,7 @@ if ($db_working) {
     // write test
     $insert_id = md5(uniqid());
     $db_write = $DB->query("INSERT INTO " . $DB->quote_identifier($RCI->config['db_prefix'] . 'session')
-        . " (`sess_id`, `created`, `ip`, `vars`) VALUES (?, ".$DB->now().", '127.0.0.1', 'foo')", $insert_id);
+        . " (`sess_id`, `changed`, `ip`, `vars`) VALUES (?, ".$DB->now().", '127.0.0.1', 'foo')", $insert_id);
 
     if ($db_write) {
       $RCI->pass('DB Write');
