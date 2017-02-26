a3924f0b3e9229dd24894ec483f8379f37c44f63
Shin Kojima <shin@kojima.org>
2017-02-27 00:41:28 +0900

    Remove checking mb_check_encoding function

--- tests/Framework/Charset.php
+++ tests/Framework/Charset.php
@@ -72,7 +72,7 @@ class Framework_Charset extends PHPUnit_Framework_TestCase
             array('Рассылки', '&BCAEMARBBEEESwQ7BDoEOA-', 'UTF-8', 'UTF7-IMAP'),
         );
 
-        if (mb_substitute_character() && function_exists('mb_check_encoding')) {
+        if (mb_substitute_character()) {
             $tests[] = array(
                 base64_decode('GyRCLWo7M3l1OSk2SBsoQg=='),
                 '㈱山﨑工業',
