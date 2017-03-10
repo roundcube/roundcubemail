d6ddd31a1bfb28e0a6033bee201caba4a56e4f5e
Thomas Bruederli <thomas@roundcube.net>
2017-03-10 10:48:16 +0100

    Better fix for XSS in style tags (9b5eee294)

--- program/lib/Roundcube/rcube_utils.php
+++ program/lib/Roundcube/rcube_utils.php
@@ -540,10 +540,10 @@ class rcube_utils
     public static function xss_entity_decode($content)
     {
         $out = html_entity_decode(html_entity_decode($content));
-        $out = strip_tags($out);
         $out = preg_replace_callback('/\\\([0-9a-f]{4})/i',
             array(self, 'xss_entity_decode_callback'), $out);
         $out = preg_replace('#/\*.*\*/#Ums', '', $out);
+        $out = strip_tags($out);
 
         return $out;
     }
