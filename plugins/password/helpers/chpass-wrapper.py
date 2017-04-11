6ba41836b6fda8f7efa197837f95a632670bbb37
Aleksander Machniak <alec@alec.pl>
2017-04-11 08:53:34 +0200

    Revert "extend plugin password to avoid sudo (use ssh instead calling chpasswd) (#5654)"
    This reverts commit 938dd4670bee27bd46fbf7d64cd0526eeb24669f.

--- plugins/password/helpers/chpass-wrapper.py
+++ plugins/password/helpers/chpass-wrapper.py
@@ -1,95 +1,32 @@
 #!/usr/bin/env python
-#
-# extended wrapper to /user/sbin/chpasswd
-#  - file based blacklist (/etc/ftpusers)
-#  - timeout for reading and writing
 
-TIMEOUT= 10
+import sys
+import pwd
+import subprocess
 
-import os, sys, pwd, re
-import subprocess, signal
-
-
-# get args for script 
-scriptargs = ''
-count=1
-while(count < len(sys.argv)):
-  # local only args, do not pass
-  try:
-    if sys.argv[count] == '-timeout':
-      count += 2
-      TIMEOUT=int(sys.argv[count-1])
-      continue
-  except ValueError:
-    continue
-  # pass all other args
-  scriptargs += ' ' + sys.argv[count]
-  count += 1
-
-# read username:password\noldpasswd with timeout
-class TimeoutException(Exception):   # Custom exception class
-    pass
-def timeout_handler(signum, frame):   # Custom signal handler
-    raise TimeoutException
-signal.signal(signal.SIGALRM, timeout_handler)
-
-# set timeout 
-signal.alarm(TIMEOUT)
-try:
-    try:
-      username, password = sys.stdin.readline().split(':', 1)
-    except ValueError, e:
-      sys.exit('Malformed input')
-
-except TimeoutException:
-  sys.exit('Timeout while reading input')
-else:
-  # clear timeout
-  signal.alarm(0)
-
-# add user to BLACKLIST and/or /etc/ftpusers to disable password change
-BLACKLIST = [
+BLACKLIST = (
     # add blacklisted users here
-    'ftp',
-]
+    #'user1',
+)
 
-# add /etc/ftpusers to BLACKLIST if exist
 try:
-  with open("/etc/ftpusers", "r") as ins:
-    for line in ins:
-      if line.startswith('#'):
-         continue
-      BLACKLIST.append(line.rstrip('\n'))
+    username, password = sys.stdin.readline().split(':', 1)
+except ValueError, e:
+    sys.exit('Malformed input')
 
-except IOError:
-  # only catch error and continue
-  pass
-
-# check if user is blacklisted for password change
-if username in BLACKLIST:
-    sys.exit('Changing password for user %s is forbidden (user blacklisted)!' %
-             username)
-
-
-# check if user exit and is allowed to chage password
 try:
     user = pwd.getpwnam(username)
 except KeyError, e:
     sys.exit('No such user: %s' % username)
 
 if user.pw_uid < 1000:
-    sys.exit('Changing the password for user %s is forbidden (system user)!' %
+    sys.exit('Changing the password for user id < 1000 is forbidden')
+
+if username in BLACKLIST:
+    sys.exit('Changing password for user %s is forbidden (user blacklisted)' %
              username)
 
-# set timeout
-signal.alarm(TIMEOUT)
-try:
-  handle = subprocess.Popen('/usr/sbin/chpasswd', stdin = subprocess.PIPE)
-  handle.communicate('%s:%s' % (username, password))
-except TimeoutException:
-  sys.exit('Timeout while changing password')
-else:
-  # clear timeout
-  signal.alarm(0)
+handle = subprocess.Popen('/usr/sbin/chpasswd', stdin = subprocess.PIPE)
+handle.communicate('%s:%s' % (username, password))
 
 sys.exit(handle.returncode)
