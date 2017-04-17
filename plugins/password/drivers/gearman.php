4425f0553aefa8b0c21cffb8022e7b82c73d66b9
root <root@srv1.innovatio.no>
2017-04-17 22:34:57 +0000

    Sync default values in config.inc.php.dist with code.
    Make sure all default values listed in config.inc.php.dist actually
    matches what the code does.

--- plugins/password/drivers/gearman.php
+++ plugins/password/drivers/gearman.php
@@ -39,7 +39,7 @@ class rcube_gearman_password
             );
 
             $gmc = new GearmanClient();
-            $gmc->addServer($rcmail->config->get('password_gearman_host'));
+            $gmc->addServer($rcmail->config->get('password_gearman_host', 'localhost'));
 
             $result  = $gmc->doNormal('setPassword', json_encode($payload));
             $success = json_decode($result);
