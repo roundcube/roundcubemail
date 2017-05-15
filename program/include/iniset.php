5101cfc67aaad37f4a02a06233f725388c4ed237
Aleksander Machniak <alec@alec.pl>
2017-05-15 11:34:43 +0200

    Bump version to 1.4-git

--- program/include/iniset.php
+++ program/include/iniset.php
@@ -5,7 +5,7 @@
  | program/include/iniset.php                                            |
  |                                                                       |
  | This file is part of the Roundcube Webmail client                     |
- | Copyright (C) 2008-2015, The Roundcube Dev Team                       |
+ | Copyright (C) 2008-2017, The Roundcube Dev Team                       |
  |                                                                       |
  | Licensed under the GNU General Public License version 3 or            |
  | any later version with exceptions for skins & plugins.                |
@@ -21,7 +21,7 @@
 */
 
 // application constants
-define('RCMAIL_VERSION', '1.3-git');
+define('RCMAIL_VERSION', '1.4-git');
 define('RCMAIL_START', microtime(true));
 
 if (!defined('INSTALL_PATH')) {
