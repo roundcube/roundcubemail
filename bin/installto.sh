46be231bc9c60884aabcaae473bf01d1c1347fee
Thomas Bruederli <thomas@roundcube.net>
2017-06-16 09:46:47 +0200

    Fix js-deps check and installation

--- bin/installto.sh
+++ bin/installto.sh
@@ -102,8 +102,8 @@ if (strtolower($input) == 'y') {
   // check if js-deps are up-to-date
   $jsdeps = json_decode(file_get_contents("$target_dir/jsdeps.json"));
   $package = $jsdeps->dependencies[0];
-  $dest_file = $target_dir . '/' . $package['dest'];
-  if (!file_exists($dest_file) || sha1_file($dest_file) !== $package['sha1']) {
+  $dest_file = $target_dir . '/' . $package->dest;
+  if (!file_exists($dest_file) || sha1_file($dest_file) !== $package->sha1) {
       echo "Installing JavaScript dependencies...";
       system("cd $target_dir && bin/install-jsdeps.sh");
       echo "done.\n\n";
