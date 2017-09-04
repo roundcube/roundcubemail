b6132b2bcd69e648f74677b741e4dfbfe7bb77cc
Thomas Bruederli <thomas@roundcube.net>
2017-09-04 20:49:32 +0200

    Bump versions and year

--- installer/index.php
+++ installer/index.php
@@ -3,7 +3,7 @@
 /**
  +-------------------------------------------------------------------------+
  | Roundcube Webmail setup tool                                            |
- | Version 1.3.0                                                           |
+ | Version 1.3.1                                                           |
  |                                                                         |
  | Copyright (C) 2009-2015, The Roundcube Dev Team                         |
  |                                                                         |
