0f0340a94084511ddfb814c9e0df5796b4c7c7e2
Aleksander Machniak <alec@alec.pl>
2017-10-27 10:11:40 +0200

    Don't set form-check class for a row with .proplist element

--- skins/elastic/ui.js
+++ skins/elastic/ui.js
@@ -489,7 +489,7 @@ function rcube_elastic_ui()
                 first.addClass('col-sm-4');
                 last.addClass('col-sm-8');
 
-                if (last.find('[type=checkbox]').length == 1) {
+                if (last.find('[type=checkbox]').length == 1 && !last.find('.proplist').length) {
                     row_classes.push('form-check');
                 }
                 else if (!last.find('input,textarea,radio,select').length) {
