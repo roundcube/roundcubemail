7f992eac3d283f29ee42fe955f01b8d4f8fbd628
Thomas Bruederli <thomas@roundcube.net>
2017-11-08 19:25:22 +0100

    Bump version + add CVE ID

--- installer/index.php
+++ installer/index.php
@@ -3,7 +3,7 @@
 /*
  +-------------------------------------------------------------------------+
  | Roundcube Webmail setup tool                                            |
- | Version 1.1.9                                                           |
+ | Version 1.1.10                                                          |
  |                                                                         |
  | Copyright (C) 2009-2016, The Roundcube Dev Team                         |
  |                                                                         |
