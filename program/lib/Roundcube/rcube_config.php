4dc1f3b757a119016fb92c7dfea972db7726fbdd
Aleksander Machniak <alec@alec.pl>
2017-11-10 20:02:17 +0100

    Use configured log_file_ext also for errors thrown by PHP (#6035)

--- program/lib/Roundcube/rcube_config.php
+++ program/lib/Roundcube/rcube_config.php
@@ -246,14 +246,14 @@ class rcube_config
 
         // set PHP error logging according to config
         if ($this->prop['debug_level'] & 1) {
-            ini_set('log_errors', 1);
+            $error_log = $this->prop['log_driver'];
+            if ($error_log != 'syslog') {
+                $error_log  = $this->prop['log_dir'] . '/errors';
+                $error_log .= isset($this->prop['log_file_ext']) ? $this->prop['log_file_ext'] : '.log';
+            }
 
-            if ($this->prop['log_driver'] == 'syslog') {
-                ini_set('error_log', 'syslog');
-            }
-            else {
-                ini_set('error_log', $this->prop['log_dir'].'/errors');
-            }
+            ini_set('error_log', $error_log);
+            ini_set('log_errors', 1);
         }
 
         // enable display_errors in 'show' level, but not for ajax requests
