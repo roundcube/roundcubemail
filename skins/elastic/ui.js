cfc408488d474b8d4bc883afd1c489f0ab80d2cf
Aleksander Machniak <machniak@kolabsys.com>
2017-12-07 13:35:57 +0100

    Add date format placeholder only to inputs with 'datepicker' class

--- skins/elastic/ui.js
+++ skins/elastic/ui.js
@@ -482,7 +482,7 @@ function rcube_elastic_ui()
 
         // Add date format placeholder to datepicker inputs
         if (rcmail.env.date_format_localized) {
-            var placeholder_func = function(input) { $(input).attr('placeholder', rcmail.env.date_format_localized); };
+            var placeholder_func = function(input) { $(input).filter('.datepicker').attr('placeholder', rcmail.env.date_format_localized); };
             $('input.datepicker').each(function() { placeholder_func(this); });
             rcmail.addEventListener('insert-edit-field', placeholder_func);
         }
