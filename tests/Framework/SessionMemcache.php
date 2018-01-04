0e1dfb27cb1a64699daafb3d50d1cbaeaff34965
Daniel Kesselberg <mail@danielkesselberg.de>
2018-01-04 23:07:01 +0100

    Add basic test

--- tests/Framework/SessionMemcache.php
+++ tests/Framework/SessionMemcache.php
@@ -10,83 +10,16 @@
 class Framework_SessionMemcache extends PHPUnit_Framework_TestCase
 {
 
-    /** @var rcube_session_memcache */
-    private $object;
-
-    /** @var rcube_config */
-    private $config;
-
-    /** @var Memcached */
-    private $memcache;
-
-    /**
-     * Sets up the fixture, for example, open a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        if (getenv('TEST_MEMCACHE') === false) {
-            $this->markTestSkipped('Tests for rcube_session_memcache disabled by default.');
-        }
-
-        $this->config = new rcube_config();
-        $this->config->set('session_storage', 'memcache');
-
-        $this->object = rcube_session::factory($this->config);
-        $this->object->set_lifetime(-60);
-    }
-
-
     /**
      * Class constructor test
      */
     public function test_class()
     {
-        $this->assertInstanceOf('rcube_session_memcache', $this->object, "Class constructor");
-    }
+        $config = new rcube_config();
+        $config->set('session_storage', 'memcache');
 
-    /**
-     * Write data to memcache and read again
-     */
-    public function test_write_and_read()
-    {
-        /** @var string $vars */
-        $vars = serialize(array('foo' => 'bar'));
-
-        $writeRequest = $this->object->write('1234', $vars);
-        $this->assertTrue($writeRequest);
-
-        $readResponse = $this->object->read('1234');
-        $this->assertEquals($vars, $readResponse);
-    }
-
-    /**
-     * Try to read data by invalid session key
-     */
-    public function test_read_not_found()
-    {
-        $readResponse = $this->object->read('5678');
-        $this->assertEmpty($readResponse);
-    }
-
-    /**
-     * Write, delete and read data
-     */
-    public function test_write_and_read_deleted()
-    {
-        /** @var string $vars */
-        $vars = serialize(array('foo' => 'bar'));
-
-        $writeRequest = $this->object->write('1234', $vars);
-        $this->assertTrue($writeRequest);
-
-        $deleteRequest = $this->object->destroy('1234');
-        $this->assertTrue($deleteRequest);
-
-        $readResponse = $this->object->read('1234');
-        $this->assertEmpty($readResponse);
+        $object = rcube_session::factory($config);
+        $this->assertInstanceOf('rcube_session_memcache', $object, "Class constructor");
     }
 
 }
