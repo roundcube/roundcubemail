6a347f36dbaa1dbd25bd817c5c77debb1a04804e
Aleksander Machniak <alec@alec.pl>
2018-04-01 18:43:28 +0200

    Elastic: Skip tables with .table class already applied

--- skins/elastic/ui.js
+++ skins/elastic/ui.js
@@ -829,7 +829,7 @@ function rcube_elastic_ui()
         });
 
         // Make tables pretier
-        $('table:not(.propform,.listing,.ui-datepicker-calendar)', context)
+        $('table:not(.table,.propform,.listing,.ui-datepicker-calendar)', context)
             .filter(function() {
                 // exclude direct propform children and external content
                 return !$(this).parent().is('.propform')
