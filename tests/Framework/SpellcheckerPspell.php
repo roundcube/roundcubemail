f31322fb3d8eb09492ef00fcde5393bcd3edd559
Rich Morgan <ionux@users.noreply.github.com>
2018-05-01 11:25:23 -0400

    Added missing method scope

--- tests/Framework/SpellcheckerPspell.php
+++ tests/Framework/SpellcheckerPspell.php
@@ -7,11 +7,10 @@
  */
 class Framework_SpellcheckerPspell extends PHPUnit_Framework_TestCase
 {
-
     /**
      * Class constructor
      */
-    function test_class()
+    public function test_class()
     {
         $object = new rcube_spellchecker_pspell(null, 'en');
 
