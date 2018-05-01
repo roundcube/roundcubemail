d4c2abb305db9bfd395c8e49bc500f5346bd9ee4
Rich Morgan <ionux@users.noreply.github.com>
2018-05-01 11:16:00 -0400

    Added missing method scope

--- tests/Framework/ImapCache.php
+++ tests/Framework/ImapCache.php
@@ -7,11 +7,10 @@
  */
 class Framework_ImapCache extends PHPUnit_Framework_TestCase
 {
-
     /**
      * Class constructor
      */
-    function test_class()
+    public function test_class()
     {
         $object = new rcube_imap_cache(new rcube_db('test'), null, null, null);
 
