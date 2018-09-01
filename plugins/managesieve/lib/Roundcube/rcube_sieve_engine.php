e9ea2f2aeb48e204028f98febb3c3e6076a392fc
Aleksander Machniak <alec@alec.pl>
2018-09-01 09:55:51 +0200

    Managesieve: Fix bug where show_real_foldernames setting wasn't respected (#6422)

--- plugins/managesieve/lib/Roundcube/rcube_sieve_engine.php
+++ plugins/managesieve/lib/Roundcube/rcube_sieve_engine.php
@@ -2405,7 +2405,6 @@ class rcube_sieve_engine
         }
 
         $select = $this->rc->folder_selector(array(
-            'realnames'  => false,
             'maxlength'  => 100,
             'id'         => 'action_mailbox' . $id,
             'name'       => "_action_mailbox[$id]",
