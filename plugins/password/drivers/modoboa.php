926e601d21f7104d6131fee4dcb5c236b399f1cb
Aleksander Machniak <alec@alec.pl>
2018-09-03 13:25:01 +0200

    Cleanup modoboa driver

--- plugins/password/drivers/modoboa.php
+++ plugins/password/drivers/modoboa.php
@@ -29,9 +29,8 @@
  * You need to define theses variables in plugin/password/config.inc.php
  *
  * $config['password_driver'] = 'modoboa'; // use modoboa as driver
- * $config['token_api_modoboa'] = ''; // put token number from Modoboa server
+ * $config['password_modoboa_api_token'] = ''; // put token number from Modoboa server
  * $config['password_minimum_length'] = 8; // select same number as in Modoboa server
- * 
  */
 
 class rcube_modoboa_password
@@ -39,44 +38,43 @@ class rcube_modoboa_password
     function save($curpass, $passwd)
     {
         // Init config access
-        $rcmail = rcmail::get_instance();    
-        $ModoboaToken = $rcmail->config->get('token_api_modoboa');
-
+        $rcmail           = rcmail::get_instance();
+        $ModoboaToken     = $rcmail->config->get('password_modoboa_api_token');
         $RoudCubeUsername = $_SESSION['username'];
-        $IMAPhost = $_SESSION['imap_host'];
+        $IMAPhost         = $_SESSION['imap_host'];
 
         // Call GET to fetch values from modoboa server
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://" . $IMAPhost . "/api/v1/accounts/?search=" . urlencode($RoudCubeUsername),
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "GET",
-          CURLOPT_HTTPHEADER => array(
-            "Authorization: Token " . $ModoboaToken,
-            "Cache-Control: no-cache",
-            "Content-Type: application/json"
-          ),
+            CURLOPT_URL            => "https://" . $IMAPhost . "/api/v1/accounts/?search=" . urlencode($RoudCubeUsername),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "GET",
+            CURLOPT_HTTPHEADER     => array(
+                "Authorization: Token " . $ModoboaToken,
+                "Cache-Control: no-cache",
+                "Content-Type: application/json"
+            ),
         ));
 
         $response = curl_exec($curl);
-        $err = curl_error($curl);
+        $err      = curl_error($curl);
 
         curl_close($curl);
 
         if ($err) {
-          return PASSWORD_CONNECT_ERROR;
+            return PASSWORD_CONNECT_ERROR;
         }
 
         // Decode json string
         $decoded = json_decode($response);
-        
+
         if (!is_array($decoded)) {
-          return PASSWORD_CONNECT_ERROR;
+            return PASSWORD_CONNECT_ERROR;
         }
 
         // Get user ID (pk)
@@ -84,36 +82,36 @@ class rcube_modoboa_password
 
         // Encode json with new password
         $ret['username'] = $decoded[0]->username;
-        $ret['role'] = $decoded[0]->role;
+        $ret['role']     = $decoded[0]->role;
         $ret['password'] = $passwd; // new password
-        $encoded = json_encode($ret);
+        $encoded         = json_encode($ret);
 
         // Call HTTP API Modoboa
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://" . $IMAPhost . "/api/v1/accounts/" . $userid . "/",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "PUT",
-          CURLOPT_POSTFIELDS => "" . $encoded . "",
-          CURLOPT_HTTPHEADER => array(
-            "Authorization: Token " . $ModoboaToken,
-            "Cache-Control: no-cache",
-            "Content-Type: application/json"
-          ),
+            CURLOPT_URL            => "https://" . $IMAPhost . "/api/v1/accounts/" . $userid . "/",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "PUT",
+            CURLOPT_POSTFIELDS     => "" . $encoded . "",
+            CURLOPT_HTTPHEADER     => array(
+                "Authorization: Token " . $ModoboaToken,
+                "Cache-Control: no-cache",
+                "Content-Type: application/json"
+            ),
         ));
 
         $response = curl_exec($curl);
-        $err = curl_error($curl);
+        $err      = curl_error($curl);
 
         curl_close($curl);
 
         if ($err) {
-          return PASSWORD_CONNECT_ERROR;
+            return PASSWORD_CONNECT_ERROR;
         }
 
         return PASSWORD_SUCCESS;
