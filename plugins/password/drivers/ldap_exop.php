ad34df7e6afc2d1c81f96edcb133bfd7727fe788
Jack Cherng <jfcherng@gmail.com>
2019-06-29 23:46:33 +0800

    Fix visibility of rcube_ldap_exop_password::$debug
    PHP Fatal error:  Access level to rcube_ldap_exop_password::$debug must be protected
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugins/password/drivers/ldap_exop.php
+++ plugins/password/drivers/ldap_exop.php
@@ -31,7 +31,7 @@ require_once __DIR__ . '/ldap_simple.php';
 
 class rcube_ldap_exop_password extends rcube_ldap_simple_password
 {
-    private $debug = false;
+    protected $debug = false;
 
     function save($curpass, $passwd)
     {
