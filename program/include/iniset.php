3d6a960bea7094563362dedf1f2309b59486220a
Aleksander Machniak <alec@alec.pl>
2019-10-27 09:45:15 +0100

    Master is 1.5-git now

--- program/include/iniset.php
+++ program/include/iniset.php
@@ -20,7 +20,7 @@
 */
 
 // application constants
-define('RCMAIL_VERSION', '1.4-git');
+define('RCMAIL_VERSION', '1.5-git');
 define('RCMAIL_START', microtime(true));
 
 if (!defined('INSTALL_PATH')) {
