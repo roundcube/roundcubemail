86cdaa0e3da6cb4c7dce6579455dfb80dea06f89
Hiroshi Shirosaki <h.shirosaki@gmail.com>
2019-12-03 17:23:45 +0900

    Fix wordwrap with ISO-2022-JP
    A message with ISO-2022-JP-MS characters currupts by wordwrap.
    Use ISO-2022-JP-MS charset in wordwrap.
    Add a unit test.

--- program/lib/Roundcube/rcube_charset.php
+++ program/lib/Roundcube/rcube_charset.php
@@ -171,6 +171,12 @@ class rcube_charset
         65001 => 'UTF-8',
     );
 
+    static public $mb_aliases = array(
+        'WINDOWS-1257' => 'ISO-8859-13',
+        'US-ASCII'     => 'ASCII',
+        'ISO-2022-JP'  => 'ISO-2022-JP-MS',
+    );
+
     /**
      * Catch an error and throw an exception.
      *
@@ -331,11 +337,7 @@ class rcube_charset
 
         // convert charset using mbstring module
         if ($mbstring_sc !== false) {
-            $aliases = array(
-                'WINDOWS-1257' => 'ISO-8859-13',
-                'US-ASCII'     => 'ASCII',
-                'ISO-2022-JP'  => 'ISO-2022-JP-MS',
-            );
+            $aliases = self::$mb_aliases;
 
             $mb_from = $aliases[$from] ?: $from;
             $mb_to   = $aliases[$to] ?: $to;
--- program/lib/Roundcube/rcube_mime.php
+++ program/lib/Roundcube/rcube_mime.php
@@ -591,6 +591,9 @@ class rcube_mime
         // Note: Never try to use iconv instead of mbstring functions here
         //       Iconv's substr/strlen are 100x slower (#1489113)
 
+        if (isset(rcube_charset::$mb_aliases[$charset])) {
+            $charset = rcube_charset::$mb_aliases[$charset];
+        }
         if ($charset && $charset != RCUBE_CHARSET) {
             mb_internal_encoding($charset);
         }
