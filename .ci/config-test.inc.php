1f656e5a073d93563e02b01d1f542dc3c4b18755
Aleksander Machniak <alec@alec.pl>
2019-12-30 11:43:42 +0100

    Travis: In-browser tests with GreenMail server

---
+++ .ci/config-test.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array();
+
+// Database configuration
+$config['db_dsnw'] = 'sqlite:////tmp/sqlite.db?mode=0646';
+
+// Test user credentials
+$config['tests_username'] = 'test';
+$config['tests_password'] = 'test';
+
+// GreenMail
+$config['smtp_port'] = 25;
+
+// Settings required by the tests
+
+$config['skin'] = 'elastic';
+$config['support_url'] = 'http://support.url';
+
