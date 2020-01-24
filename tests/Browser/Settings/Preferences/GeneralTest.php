8080eb2c71838d82bde2c1285146d805c0a88094
Aleksander Machniak <alec@alec.pl>
2020-01-24 21:28:07 +0100

    Tests: Fix database locking bug

--- tests/Browser/Settings/Preferences/GeneralTest.php
+++ tests/Browser/Settings/Preferences/GeneralTest.php
@@ -146,18 +146,18 @@ class GeneralTest extends \Tests\Browser\TestCase
                 $browser->assertCheckboxState('_pretty_date', $this->settings['pretty_date']);
                 $browser->assertCheckboxState('_display_next', $this->settings['display_next']);
                 $browser->assertCheckboxState('_standard_windows', $this->settings['standard_windows']);
-
-                // Assert the options have been saved in database properly
-                $prefs   = \bootstrap::get_prefs();
-                $options = array_diff(array_keys($this->settings), ['refresh_interval', 'pretty_date']);
-
-                foreach ($options as $option) {
-                    $this->assertEquals($this->settings[$option], $prefs[$option]);
-                }
-
-                $this->assertEquals($this->settings['pretty_date'], $prefs['prettydate']);
-                $this->assertEquals($this->settings['refresh_interval'], $prefs['refresh_interval']/60);
             });
         });
+
+        // Assert the options have been saved in database properly
+        $prefs   = \bootstrap::get_prefs();
+        $options = array_diff(array_keys($this->settings), ['refresh_interval', 'pretty_date']);
+
+        foreach ($options as $option) {
+            $this->assertEquals($this->settings[$option], $prefs[$option]);
+        }
+
+        $this->assertEquals($this->settings['pretty_date'], $prefs['prettydate']);
+        $this->assertEquals($this->settings['refresh_interval'], $prefs['refresh_interval']/60);
     }
 }
