e1ced614ee2fc843fe0e78d38e7c7d089c7119bf
Aleksander Machniak <alec@alec.pl>
2020-02-23 19:05:21 +0100

    Tests: Add couple of HTML editor tests (including bug #7230)

--- tests/Browser/Mail/ComposeTest.php
+++ tests/Browser/Mail/ComposeTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Browser\Mail;
 
+use Facebook\Webdriver\WebDriverKeys;
 use Tests\Browser\Components\App;
+use Tests\Browser\Components\HtmlEditor;
 
 class ComposeTest extends \Tests\Browser\TestCase
 {
@@ -63,6 +65,37 @@ class ComposeTest extends \Tests\Browser\TestCase
             // Compose options
             $browser->assertSeeIn('#layout-sidebar .header', 'Options and attachments');
             $browser->assertVisible('#compose-attachments');
+        });
+    }
+
+    /**
+     * depends @testCompose
+     */
+    function testPlainEditor()
+    {
+        // Test for #7230: Shift+PageUp text selection
+        // and copy-pasting with keyboard
+        $this->browse(function ($browser) {
+            $browser->with(new HtmlEditor('composebodycontainer'), function ($browser) {
+                $browser->assertMode(HtmlEditor::MODE_PLAIN)
+                    ->type('@plain-body', "line1\nline2\n")
+                    ->keys('@plain-body', [WebDriverKeys::SHIFT, WebDriverKeys::PAGE_UP])
+                    ->keys('@plain-body', [WebDriverKeys::CONTROL, 'c'])
+                    ->keys('@plain-body', [WebDriverKeys::CONTROL, 'x'])
+                    ->keys('@plain-body', [WebDriverKeys::CONTROL, 'v'])
+                    ->keys('@plain-body', [WebDriverKeys::CONTROL, 'v'])
+                    ->assertValue('@plain-body', "line1\nline2\nline1\nline2\n");
+            });
+        });
+
+        // Test switching to HTML and back
+        $this->browse(function ($browser) {
+            $browser->with(new HtmlEditor('composebodycontainer'), function ($browser) {
+                $browser->switchMode(HtmlEditor::MODE_HTML, true)
+                    ->switchMode(HtmlEditor::MODE_PLAIN)
+                    ->assertValue('@plain-body', "line1\nline2\nline1\nline2")
+                    ->switchMode(HtmlEditor::MODE_HTML, false);
+            });
         });
     }
 }
