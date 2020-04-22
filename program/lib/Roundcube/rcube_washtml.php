399d9cb2b26fb4cfc93b066a5a592ed5fce5379c
Aleksander Machniak <alec@alec.pl>
2020-04-22 12:36:51 +0200

    Fix typo

--- program/lib/Roundcube/rcube_washtml.php
+++ program/lib/Roundcube/rcube_washtml.php
@@ -598,7 +598,7 @@ class rcube_washtml
         if (!$this->is_xml && class_exists('Masterminds\HTML5')
             // HTML5 parser is slow with content that contains a lot of tags
             // disable it for such cases (https://github.com/Masterminds/html5-php/issues/181)
-            && substr_count($html, '<') > 10000
+            && substr_count($html, '<') < 10000
         ) {
             try {
                 $html5 = new Masterminds\HTML5();
