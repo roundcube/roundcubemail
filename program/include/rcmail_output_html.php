ecabb1e667a1cd62e5141fcb16dc69ac17bb82c1
Aleksander Machniak <alec@alec.pl>
2020-05-30 08:28:05 +0200

    Security: Fix XSS issue in template object 'username' (#7406)

--- program/include/rcmail_output_html.php
+++ program/include/rcmail_output_html.php
@@ -2134,7 +2134,9 @@ EOF;
             $username = $this->app->user->get_username();
         }
 
-        return rcube_utils::idn_to_utf8($username);
+        $username = rcube_utils::idn_to_utf8($username);
+
+        return html::quote($username);
     }
 
     /**
