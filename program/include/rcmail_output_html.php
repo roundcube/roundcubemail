57a435e71864c99d2d607f93f25e839c5bcfbf68
PhilW <roundcube@tehinterweb.co.uk>
2020-07-04 08:58:46 +0100

    fix skin_logo config, again

--- program/include/rcmail_output_html.php
+++ program/include/rcmail_output_html.php
@@ -1376,10 +1376,7 @@ EOF;
                 else if ($object == 'logo') {
                     $attrib += array('alt' => $this->xml_command(array('', 'object', 'name="productname"')));
 
-                    if (!empty($attrib['type']) && ($template_logo = $this->get_template_logo($attrib['type'])) !== null) {
-                        $attrib['src'] = $template_logo;
-                    }
-                    else if (($template_logo = $this->get_template_logo()) !== null) {
+                    if (($template_logo = $this->get_template_logo($attrib['logo-type'], $attrib['logo-match'])) !== null) {
                         $attrib['src'] = $template_logo;
                     }
 
@@ -2491,12 +2488,14 @@ EOF;
     /**
      * Get logo URL for current template based on skin_logo config option
      *
-     * @param string  $type     Type of the logo to check for (e.g. 'print' or 'small')
-     *                          default is null (no special type)
+     * @param string $type   Type of the logo to check for (e.g. 'print' or 'small')
+     *                       default is null (no special type)
+     * @param string $match  (optional) 'all' = type, template or wildcard, 'template' = type or template
+     *                       Note: when type is specified matches are limited to type only unless $match is defined
      *
      * @return string image URL
      */
-    protected function get_template_logo($type = null)
+    protected function get_template_logo($type = null, $match = null)
     {
         $template_logo = null;
 
@@ -2519,13 +2518,18 @@ EOF;
                     '*',
                 );
 
-                if (!empty($type)) {
-                    // Use strict matching, remove wild card options
-                    $template_names = preg_grep("/\*$/", $template_names, PREG_GREP_INVERT);
+                if (empty($type)) {
+                    // If no type provided then remove those options from the list
+                    $template_names = preg_grep("/\]$/", $template_names, PREG_GREP_INVERT);
                 }
-                else {
-                    // No type set so remove those options from the list
-                    $template_names = preg_grep("/\\[\]$/", $template_names, PREG_GREP_INVERT);
+                elseif ($match === null) {
+                    // Type specified with no special matching requirements so remove all none type specific options from the list
+                    $template_names = preg_grep("/\]$/", $template_names);
+                }
+
+                if ($match == 'template') {
+                    // Match only specific type or template name
+                    $template_names = preg_grep("/\*$/", $template_names, PREG_GREP_INVERT);
                 }
 
                 foreach ($template_names as $key) {
