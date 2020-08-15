d0b3411f76302d4b70d3605920d2abc29d71a334
Aleksander Machniak <alec@alec.pl>
2020-08-15 12:44:07 +0200

    Fix an obvious code bugs

--- plugins/enigma/lib/enigma_driver_gnupg.php
+++ plugins/enigma/lib/enigma_driver_gnupg.php
@@ -197,7 +197,7 @@ class enigma_driver_gnupg extends enigma_driver
         try {
             $this->gpg->addSignKey($key->reference, $key->password);
 
-            $res     = $this->gpg->sign($text, $mode, CRYPT_GPG::ARMOR_ASCII, true);
+            $res     = $this->gpg->sign($text, $mode, Crypt_GPG::ARMOR_ASCII, true);
             $sigInfo = $this->gpg->getLastSignatureInfo();
 
             $this->last_sig_algorithm = $sigInfo->getHashAlgorithmName();
