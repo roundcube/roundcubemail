bf2f063d03d9b178ebda5b692d725afb88cd666a
Aleksander Machniak <alec@alec.pl>
2020-09-22 19:26:38 +0200

    Fix HTML editor in latest Chrome 85.0.4183.102, update to TinyMCE 4.9.11 (#7615)

--- program/js/editor.js
+++ program/js/editor.js
@@ -39,7 +39,7 @@ function rcube_text_editor(config, id)
     abs_url = location.href.replace(/[?#].*$/, '').replace(/\/$/, ''),
     conf = {
       selector: '#' + ($('#' + id).is('.mce_editor') ? id : 'fake-editor-id'),
-      cache_suffix: 's=4080200',
+      cache_suffix: 's=40091100',
       theme: 'modern',
       language: config.lang,
       content_css: rcmail.assets_path(config.content_css),
