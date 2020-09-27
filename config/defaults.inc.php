8003e8dbda774002074959459fb02e30d50d0d55
Aleksander Machniak <alec@alec.pl>
2020-09-27 11:32:06 +0200

    Contact form mode: private/business (#7630)

--- config/defaults.inc.php
+++ config/defaults.inc.php
@@ -1173,6 +1173,11 @@ $config['contactlist_fields'] = array('name', 'firstname', 'surname', 'email');
 // See program/steps/addressbook/func.inc for a list
 $config['contact_search_name'] = '{name} <{email}>';
 
+// Contact mode. If your contacts are mostly business, switch it to 'business'.
+// This will prioritize form fields related to 'work' (instead of 'home').
+// Default: 'private'.
+$config['contact_form_mode'] = 'private';
+
 // The addressbook source to store automatically collected recipients in.
 // Default: true (the built-in "Collected recipients" addressbook, source id = '1')
 // Note: It can be set to any writeable addressbook, e.g. 'sql'
