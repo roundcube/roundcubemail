51480044d6d9a9d808160aac34527d7c91599c74
Thomas Bruederli <thomas@roundcube.net>
2020-12-27 22:10:48 +0100

    Bump version to 1.2.13

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  +-------------------------------------------------------------------------+
  | Roundcube Webmail IMAP Client                                           |
- | Version 1.2.12                                                          |
+ | Version 1.2.13                                                          |
  |                                                                         |
  | Copyright (C) 2005-2017, The Roundcube Dev Team                         |
  |                                                                         |
--- installer/index.php
+++ installer/index.php
@@ -3,7 +3,7 @@
 /**
  +-------------------------------------------------------------------------+
  | Roundcube Webmail setup tool                                            |
- | Version 1.2.12                                                          |
+ | Version 1.2.13                                                          |
  |                                                                         |
  | Copyright (C) 2009-2017, The Roundcube Dev Team                         |
  |                                                                         |
