e263aad68f7686a7679e16599553f0f3a63bc1d8
PhilW <roundcube@tehinterweb.co.uk>
2020-12-27 15:54:04 +0000

    get minifying tools from npm

--- bin/cssshrink.sh
+++ bin/cssshrink.sh
@@ -3,37 +3,17 @@
 set -e
 
 PWD=`dirname "$0"`
-JAR_DIR='/tmp'
-VERSION='2.4.8'
-COMPILER_URL="https://github.com/yui/yuicompressor/releases/download/v${VERSION}/yuicompressor-${VERSION}.zip"
 
 do_shrink() {
     rm -f "$2"
-    java -jar $JAR_DIR/yuicompressor.jar -v -o "$2" "$1"
+    yuicompressor -v -o "$2" "$1"
 }
 
-if [ ! -w "$JAR_DIR" ]; then
-    JAR_DIR=$PWD
-fi
-
-if java -version >/dev/null 2>&1; then
+if which yuicompressor > /dev/null 2>&1; then
     :
 else
-    echo "Java not found. Please ensure that the 'java' program is in your PATH."
+    echo "yuicompressor not found. Please install e.g. 'npm install yuicompressor'."
     exit 1
-fi
-
-if [ ! -r "$JAR_DIR/yuicompressor.jar" ]; then
-    if which wget >/dev/null 2>&1 && which unzip >/dev/null 2>&1; then
-        wget "$COMPILER_URL" -O "/tmp/$$.zip"
-    elif which curl >/dev/null 2>&1 && which unzip >/dev/null 2>&1; then
-        curl -L "$COMPILER_URL" -o "/tmp/$$.zip"
-    else
-        echo "Please download $COMPILER_URL and extract compiler.jar to $JAR_DIR/."
-        exit 1
-    fi
-    (cd $JAR_DIR && unzip "/tmp/$$.zip" && mv "yuicompressor-${VERSION}.jar" "yuicompressor.jar")
-    rm -f "/tmp/$$.zip"
 fi
 
 # compress single file from argument
