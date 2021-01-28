4cfe256394d98c47076ce4548f88af12d1199527
Marco Miltenburg <marco@virtualpages.nl>
2021-01-28 22:00:00 +0100

    Fixed syntax mistakes in MSSQL initial database script

--- SQL/mssql.initial.sql
+++ SQL/mssql.initial.sql
@@ -47,7 +47,7 @@ CREATE TABLE [dbo].[collected_addresses] (
 	[name] [varchar] (255) COLLATE Latin1_General_CI_AI NOT NULL ,
 	[email] [varchar] (255) COLLATE Latin1_General_CI_AI NOT NULL ,
 	[type] [int] NOT NULL 
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
 CREATE TABLE [dbo].[contacts] (
@@ -297,7 +297,7 @@ GO
 ALTER TABLE [dbo].[collected_addresses] ADD 
 	CONSTRAINT [DF_collected_addresses_user_id] DEFAULT (0) FOR [user_id],
 	CONSTRAINT [DF_collected_addresses_changed] DEFAULT (getdate()) FOR [changed],
-	CONSTRAINT [DF_collected_addresses_name] DEFAULT ('') FOR [name],
+	CONSTRAINT [DF_collected_addresses_name] DEFAULT ('') FOR [name]
 GO
 
 CREATE UNIQUE INDEX [IX_collected_addresses_user_id] ON [dbo].[collected_addresses]([user_id],[type],[email]) ON [PRIMARY]
