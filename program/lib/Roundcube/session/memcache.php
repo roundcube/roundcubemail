be9836334e52639dc71824da097fe16e93d8501a
Josh Soref <jsoref@users.noreply.github.com>
2021-04-13 00:01:40 -0400

    spelling: operation
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- program/lib/Roundcube/session/memcache.php
+++ program/lib/Roundcube/session/memcache.php
@@ -185,7 +185,7 @@ class rcube_session_memcache extends rcube_session
      * @param string $type   Operation type
      * @param string $key    Session identifier
      * @param string $data   Data to log
-     * @param bool   $result Opearation result
+     * @param bool   $result Operation result
      */
     protected function debug($type, $key, $data = null, $result = null)
     {
