2f57ef76bd5c898f3c1bf13bacc27c099afed53f
Josh Soref <jsoref@users.noreply.github.com>
2021-04-13 00:01:16 -0400

    spelling: instructions
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- bin/update.sh
+++ bin/update.sh
@@ -281,7 +281,7 @@ if ($RCI->configured) {
 }
 else {
     echo "This instance of Roundcube is not yet configured!\n";
-    echo "Open http://url-to-roundcube/installer/ in your browser and follow the instuctions.\n";
+    echo "Open http://url-to-roundcube/installer/ in your browser and follow the instructions.\n";
 }
 
 function repo_key($repo)
