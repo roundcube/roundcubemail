529f9e92e6e240ca92c485c8afef5b463480c5ae
Aleksander Machniak <alec@alec.pl>
2021-07-20 11:04:46 +0200

    Remove redundant if statement (#8119)

--- program/actions/mail/attachment_delete.php
+++ program/actions/mail/attachment_delete.php
@@ -37,11 +37,9 @@ class rcmail_action_mail_attachment_delete extends rcmail_action_mail_attachment
         if (is_array($attachment)) {
             $attachment = $rcmail->plugins->exec_hook('attachment_delete', $attachment);
 
-            if ($attachment['status']) {
-                if (!empty(self::$COMPOSE['attachments'][self::$file_id])) {
-                    $rcmail->session->remove(self::$SESSION_KEY . '.attachments.' . self::$file_id);
-                    $rcmail->output->command('remove_from_attachment_list', 'rcmfile' . self::$file_id);
-                }
+            if (!empty($attachment['status'])) {
+                $rcmail->session->remove(self::$SESSION_KEY . '.attachments.' . self::$file_id);
+                $rcmail->output->command('remove_from_attachment_list', 'rcmfile' . self::$file_id);
             }
         }
 
