ac4134306d8e225663c4cfae2e567ea9ba130a17
Robin Friberg <robin@devpeak.se>
2021-08-01 10:35:30 +0200

    Fix dovecot passwdfile change (#8145)
    * Fix dovecot passwd-file handler
    * Set new password hash correctly
    * If password hashing failes during password change request, abort trying to change the password.
    * Escaping hashed password is not needed and actually breaks if 'password_dovecotpw_with_method' is set to true (escaping the {METHOD} in the passwd file)

--- plugins/password/drivers/dovecot_passwdfile.php
+++ plugins/password/drivers/dovecot_passwdfile.php
@@ -40,7 +40,18 @@ class rcube_dovecot_passwdfile_password
         $mailuserfile = $rcmail->config->get('password_dovecot_passwdfile_path') ?: '/etc/mail/imap.passwd';
 
         $password = password::hash_password($newpass);
-        $password = escapeshellcmd($password); // FIXME: Do we need this?
+
+        if ($password === false) {
+            rcube::raise_error([
+                    'code' => 600, 'file' => __FILE__, 'line' => __LINE__,
+                    'message' => "Password plugin: Failed to hash password. Check for configuration issues."
+                ],
+                true, false
+            );
+
+            return PASSWORD_ERROR;
+        }
+
         $username = escapeshellcmd($username); // FIXME: Do we need this?
         $content  = '';
 
@@ -60,10 +71,10 @@ class rcube_dovecot_passwdfile_password
 
         if (flock($fp, LOCK_EX)) {
             // Read the file and replace the user password
-            while (($line = fgets($handle, 40960)) !== false) {
+            while (($line = fgets($fp, 40960)) !== false) {
                 if (strpos($line, "$username:") === 0) {
                     $pos  = strpos($line, ':', strlen("$username:") + 1);
-                    $line = "$username:$newhash" . substr($line, $pos);
+                    $line = "$username:$password" . substr($line, $pos);
                 }
 
                 $content .= $line;
