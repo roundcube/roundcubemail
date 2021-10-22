d1f16ebb97aefe76951eb6bdb1bb2eaa6d991b45
Aleksander Machniak <alec@alec.pl>
2021-10-22 18:30:58 +0200

    Fix so dark mode state is retained after closing the browser (#8237)

--- program/js/app.js
+++ program/js/app.js
@@ -10103,6 +10103,11 @@ function rcube_webmail()
   // Cookie setter
   this.set_cookie = function(name, value, expires)
   {
+    if (expires === false) {
+      var expires = new Date();
+      expires.setYear(expires.getFullYear() + 1);
+    }
+
     setCookie(name, value, expires, this.env.cookie_path, this.env.cookie_domain, this.env.cookie_secure);
   };
 
