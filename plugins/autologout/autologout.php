2ece2b23278c68d530556b99eda195edd6327cc1
CoverTowerSuperAdmin <86950868+CoverTowerSuperAdmin@users.noreply.github.com>
2021-10-28 11:04:03 +0300

    Create autologout.php

---
+++ plugins/autologout/autologout.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Plugin to auto log out
+ * users with a POST request
+ * sent by submitting a form
+ *
+ * @license GNU GPLv3+
+ * @author  Cover Tower LLC
+ *
+ * First enable this plugin by setting $config['plugins'] = array(..., 'autologout')
+ * in the Roundcube configuration file (config.inc.php). To use it, just embed
+ * a form like the following in a web page:
+ * <form id="rcLogoutForm" method="POST" action="https://mail.example.com/">
+ * <input type="hidden" name="_action" value="logout" />
+ * <input type="hidden" name="_task" value="logout" />
+ * <input type="hidden" name="_autologout" value="1" />
+ * <input id="loSubmitButton" type="submit" value="Logout" />
+ * </form>
+ */
+
+class autologout extends rcube_plugin
+{
+  public $task = 'logout';
+
+  function init()
+  {
+    $this->add_hook('startup', array($this, 'startup'));
+  }
+
+  function startup($args)
+  {
+    $rcmail = rcmail::get_instance();
+
+    // Change task and action to logout
+    if (!empty($_SESSION['user_id']) && !empty($_POST['_autologout']) && $this->known_client())
+    {
+        $rcmail->logout_actions();
+        $rcmail->kill_session();
+    }
+
+    return $args;
+  }
+
+  function known_client()
+  {
+     /**
+      *  If you want to restrict the use of this plugin to specific
+      *  remote clients, you can verify the remote client's IP like this:
+      *
+      *  if (in_array($_SERVER['REMOTE_ADDR'], ['123.123.123.123', '124.124.124.124'])) { return true; }
+      */
+
+     return true;
+  }
+
+}
