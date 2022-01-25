b82ae59da04622c2f6bbce372c124e9c851bb806
Thomas Bruederli <thomas@roundcube.net>
2022-01-25 22:11:00 +0100

    Add utils script to initialize or update database
    This can be used by the Docker images where
    we don't know whether the confgured database
    is already initialized.

---
+++ bin/init-or-update-db.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env php
+<?php
+/*
+ +-----------------------------------------------------------------------+
+ | This file is part of the Roundcube Webmail client                     |
+ |                                                                       |
+ | Copyright (C) The Roundcube Dev Team                                  |
+ |                                                                       |
+ | Licensed under the GNU General Public License version 3 or            |
+ | any later version with exceptions for skins & plugins.                |
+ | See the README file for a full license statement.                     |
+ |                                                                       |
+ | PURPOSE:                                                              |
+ |   Create or update database schema                                    |
+ +-----------------------------------------------------------------------+
+ | Author: Thomas Bruederli <thomas@roundcube.net>                       |
+ +-----------------------------------------------------------------------+
+*/
+
+define('INSTALL_PATH', realpath(__DIR__ . '/..') . '/' );
+
+require_once INSTALL_PATH . 'program/include/clisetup.php';
+
+// get arguments
+$opts = rcube_utils::get_opt([
+    'd' => 'dir',
+]);
+
+if (empty($opts['dir'])) {
+    rcube::raise_error("Database schema directory not specified (--dir).", false, true);
+}
+
+// Check if directory exists
+if (!file_exists($opts['dir'])) {
+    rcube::raise_error("Specified database schema directory doesn't exist.", false, true);
+}
+
+$db = rcmail_utils::db();
+
+if (in_array($db->table_name('system'), (array)$db->list_tables())) {
+  echo "Checking for database schema updates..." . PHP_EOL;
+  rcmail_utils::db_update($opts['dir'], 'roundcube', null, ['errors' => true]);
+} else {
+  rcmail_utils::db_init($opts['dir']);
+}
