c7f53a35d05323d822bd40ccc1761df9065d73d2
Aleksander Machniak <alec@alec.pl>
2022-08-12 19:45:40 +0200

    Fix bug where it wasn't possible to create more than one response record on SQLite and Postgres (#8664)

--- SQL/mysql.initial.sql
+++ SQL/mysql.initial.sql
@@ -260,4 +260,4 @@ CREATE TABLE `system` (
 
 SET FOREIGN_KEY_CHECKS=1;
 
-INSERT INTO `system` (`name`, `value`) VALUES ('roundcube-version', '2021100300');
+INSERT INTO `system` (`name`, `value`) VALUES ('roundcube-version', '2022081200');
