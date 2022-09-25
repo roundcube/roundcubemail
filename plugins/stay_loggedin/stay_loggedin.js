b73fcd6843c1e9d41b676b55632c3b0366f799f7
Github-Citizen <80302627+Github-Citizen@users.noreply.github.com>
2022-09-25 11:13:18 -0400

    Update stay_loggedin.js

--- plugins/stay_loggedin/stay_loggedin.js
+++ plugins/stay_loggedin/stay_loggedin.js
@@ -8,13 +8,13 @@ $(document).ready(function () {
 
 			let html = '';
 			html += '<tr class="form-group row">';
-			html += '<td class="title" style="display: none;"></td>';
-			html += '<td class="input input-group input-group-lg">';
-			html +=   '<div class="custom-control custom-switch" style="padding: 1em 0;">';
-			html +=     '<input type="checkbox" class="custom-control-input" id="_stay_loggedin" name="_stay_loggedin" value="1">';
-			html +=     '<label class="custom-control-label" for="_stay_loggedin">' + txt + '</label>';
-			html +=   '</div>';
-			html += '</td>';
+			html +=   '<td class="title" style="display: none;"></td>';
+			html +=   '<td class="input input-group input-group-lg">';
+			html +=     '<div class="custom-control custom-switch" style="padding: 1em 0;">';
+			html +=       '<input type="checkbox" class="custom-control-input" id="_stay_loggedin" name="_stay_loggedin" value="1">';
+			html +=       '<label class="custom-control-label" for="_stay_loggedin">' + txt + '</label>';
+			html +=     '</div>';
+			html +=   '</td>';
 			html += '</tr>';
 
 			let element = $('#login-form table tbody');
