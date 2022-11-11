e111f22d507f9fce7b06449e4dce922d9c773dc6
Aleksander Machniak <alec@alec.pl>
2022-11-11 13:26:08 +0100

    Fix a PHP warning (#8743)

--- program/lib/Roundcube/rcube_imap.php
+++ program/lib/Roundcube/rcube_imap.php
@@ -2064,7 +2064,7 @@ class rcube_imap extends rcube_storage
                     $struct->ctype_secondary = strtolower($part[$i]);
 
                     // read content type parameters
-                    if (is_array($part[$i+1])) {
+                    if (isset($part[$i+1]) && is_array($part[$i+1])) {
                         $struct->ctype_parameters = [];
                         for ($j=0; $j<count($part[$i+1]); $j+=2) {
                             $param = strtolower($part[$i+1][$j]);
