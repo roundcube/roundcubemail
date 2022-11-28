872435e0def509d5c203620383582dbdf5ae19d0
Aleksander Machniak <alec@alec.pl>
2022-11-28 20:34:02 +0100

    Fix PHP 8.1 warnings, make BODYSTRUCTURE parser less fragile (#8803)

--- tests/Framework/Imap.php
+++ tests/Framework/Imap.php
@@ -99,4 +99,45 @@ class Framework_Imap extends PHPUnit\Framework\TestCase
 
         $this->assertSame($expected, $result);
     }
+
+    /**
+     * BODYSTRUCTURE parsing
+     */
+    function test_bodystructure()
+    {
+        // A sample from #8803
+        $str = '(("TEXT" "PLAIN" ("CHARSET" "utf-8") NIL NIL "8bit" 232 7)'
+            . '("MESSAGE" "DISPOSITION-NOTIFICATION" ("NAME" "ATT-3.dat") NIL NIL "7bit" 269 NIL ("ATTACHMENT" ("FILENAME" "ATT-3.dat")))'
+            . '("MESSAGE" "RFC822" ("NAME" "Test mail.eml") NIL NIL "7bit" 3953 ("Fri, 25 Nov 2022 18:08:05 +0000" "Test mail"'
+                . ' (("Sender" NIL "sender" "hostname.tld"))'
+                . ' (("Sender" NIL "sender" "hostname.tld"))'
+                . ' (("Sender" NIL "sender" "hostname.tld"))'
+                . ' (("Recipient A" NIL "extmail" "exthost.tld"))'
+                . ' (("Recipient B" NIL "otherusr" "hostname.tld"))'
+                . ' NIL NIL "<960564af959918c2a7b2e59bde1ebb79@hostname.tld>")'
+                . ' ( "MIXED" ("BOUNDARY" "=_0cc01990d46dea96cd7d692970fcbf82") NIL NIL) 1 NIL ("ATTACHMENT" ("FILENAME" "Test mail.eml")))'
+            . ' "REPORT" ("BOUNDARY" "=_RrjQxjLYBqTMnoYWobuYlwN") NIL NIL)';
+
+        $structure = rcube_imap_generic::tokenizeResponse($str, 1);
+
+        $imap = new rcube_imap();
+
+        $result = invokeMethod($imap, 'structure_part', [$structure]);
+
+        $this->assertSame('0', $result->mime_id);
+        $this->assertSame('multipart', $result->ctype_primary);
+        $this->assertSame('report', $result->ctype_secondary);
+        $this->assertSame('multipart/report', $result->mimetype);
+        $this->assertSame(['boundary' => '=_RrjQxjLYBqTMnoYWobuYlwN'], $result->ctype_parameters);
+        $this->assertSame([], $result->d_parameters);
+        $this->assertSame('8bit', $result->encoding);
+        $this->assertCount(3, $result->parts);
+
+        $part = $result->parts[2];
+        $this->assertSame('3', $part->mime_id);
+        $this->assertSame('message/rfc822', $part->mimetype);
+        $this->assertSame('multipart/mixed', $part->real_mimetype);
+        $this->assertSame(3953, $part->size);
+        $this->assertCount(1, $part->parts);
+    }
 }
