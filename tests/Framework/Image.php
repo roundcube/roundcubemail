18aadd1c276b0bc6d872fb3bc3dba6473cb5a350
Aleksander Machniak <alec@alec.pl>
2023-04-10 12:32:14 +0200

    Support conversion from webp formt using GD, improve is_convertable()

--- tests/Framework/Image.php
+++ tests/Framework/Image.php
@@ -74,4 +74,55 @@ class Framework_Image extends PHPUnit\Framework\TestCase
         $this->assertSame(64, $props['width']);
         $this->assertSame(64, $props['height']);
     }
+
+    /**
+     * Test is_convertable() method
+     */
+    function test_convertable()
+    {
+        rcube::get_instance()->config->set('im_convert_path', '');
+
+        $file = rcube_utils::temp_filename('tests');
+        $object = new rcube_image($file);
+
+        if (class_exists('Imagick', false)) {
+            $this->assertTrue($object->is_convertable('image/gif'));
+            $this->assertFalse($object->is_convertable('xxx'));
+        }
+        else if (!function_exists('getimagesize')) {
+            $this->markTestSkipped();
+        }
+
+        if (function_exists('imagecreatefromgif')) {
+            $this->assertTrue($object->is_convertable('image/gif'));
+        }
+        else {
+            $this->assertFalse($object->is_convertable('image/gif'));
+        }
+
+        if (function_exists('imagecreatefromjpeg')) {
+            $this->assertTrue($object->is_convertable('image/jpg'));
+            $this->assertTrue($object->is_convertable('image/jpeg'));
+        }
+        else {
+            $this->assertFalse($object->is_convertable('image/jpg'));
+            $this->assertFalse($object->is_convertable('image/jpeg'));
+        }
+
+        if (function_exists('imagecreatefrompng')) {
+            $this->assertTrue($object->is_convertable('image/png'));
+        }
+        else {
+            $this->assertFalse($object->is_convertable('image/png'));
+        }
+
+        if (function_exists('imagecreatefromwebp')) {
+            $this->assertTrue($object->is_convertable('image/webp'));
+        }
+        else {
+            $this->assertFalse($object->is_convertable('image/webp'));
+        }
+
+        $this->assertFalse($object->is_convertable('xxx'));
+    }
 }
