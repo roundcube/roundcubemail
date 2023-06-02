507a902f504241eb22d39f3cbbb318223ab5f8ce
Aleksander Machniak <alec@alec.pl>
2023-06-02 13:16:12 +0200

    Fix PHP8 warnings

--- plugins/acl/acl.php
+++ plugins/acl/acl.php
@@ -149,8 +149,8 @@ class acl extends rcube_plugin
      */
     function folder_form($args)
     {
-        $mbox_imap = $args['options']['name'];
-        $myrights  = $args['options']['rights'];
+        $mbox_imap = isset($args['options']['name']) ? $args['options']['name'] : '';
+        $myrights  = isset($args['options']['rights']) ? $args['options']['rights'] : null;
 
         // Edited folder name (empty in create-folder mode)
         if (!strlen($mbox_imap)) {
