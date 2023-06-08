e25608faae05a7fac411fac2c62c4c79f80b98ce
Aleksander Machniak <alec@alec.pl>
2023-06-08 20:43:07 +0200

    Remove redundant comma

--- program/actions/mail/compose.php
+++ program/actions/mail/compose.php
@@ -1606,7 +1606,7 @@ class rcmail_action_mail_compose extends rcmail_action_mail_index
                     'onclick'      => sprintf(
                         "return %s.command('insert-response', '%s', this, event)",
                         rcmail_output::JS_OBJECT_NAME,
-                        rcube::JQ($response['id']),
+                        rcube::JQ($response['id'])
                     ),
                 ],
                 rcube::Q($response['name'])
