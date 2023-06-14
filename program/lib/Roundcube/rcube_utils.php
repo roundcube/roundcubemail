5a44e539fd7c6b599682bbd7e1b2b818d7f46bcb
Aleksander Machniak <alec@alec.pl>
2023-06-14 13:00:54 +0200

    Fix PHP7 compat. break in last commit

--- program/lib/Roundcube/rcube_utils.php
+++ program/lib/Roundcube/rcube_utils.php
@@ -1487,7 +1487,7 @@ class rcube_utils
 
                 return $date->format($format);
             }
-            catch (Exception) {
+            catch (Exception $e) {
                 // ignore, fallback to date()
             }
         }
