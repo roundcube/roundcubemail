dc7b6850c68870570b438d79c0949a5031522127
Aleksander Machniak <alec@alec.pl>
2023-10-14 18:23:27 +0200

    Fix merge conflict

--- program/lib/Roundcube/rcube_washtml.php
+++ program/lib/Roundcube/rcube_washtml.php
@@ -412,13 +412,9 @@ class rcube_washtml
                 return $this->config['blocked_src'];
             }
         }
-<<<<<<< HEAD
-        else if ($is_image && preg_match('/^data:image\/([^,]+),(.+)$/i', $uri, $matches)) { // RFC2397
-=======
         else if ($is_image && preg_match('/^data:image\/([^,]+),(.+)$/is', $uri, $matches)) { // RFC2397
             $type = preg_replace('/\s/', '', $matches[1]);
 
->>>>>>> 6ee6e7ae3... Fix cross-site scripting (XSS) vulnerability in handling of SVG in HTML messages (#9168)
             // svg images can be insecure, we'll sanitize them
             if (stripos($type, 'svg') !== false) {
                 $svg = $matches[2];
