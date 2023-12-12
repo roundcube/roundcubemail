58e30ef50608cad20ee2bdb9c4c254c2540f5452
Michael Voříšek <mvorisek@mvorisek.cz>
2023-12-12 20:59:23 +0100

    fix method calls case

--- program/lib/Roundcube/cache/redis.php
+++ program/lib/Roundcube/cache/redis.php
@@ -212,7 +212,7 @@ class rcube_cache_redis extends rcube_cache
         }
 
         try {
-            $result = self::$redis->setEx($key, $this->ttl, $data);
+            $result = self::$redis->setex($key, $this->ttl, $data);
         }
         catch (Exception $e) {
             rcube::raise_error($e, true, false);
