3dbbbd4ce19d8cc3e03d82be4caa6313c4f6a3d3
Aleksander Machniak <alec@alec.pl>
2024-03-09 11:58:33 +0100

    Fix attachment name decoding when 'charset' parameter exists in the headers (#9376)

--- program/lib/Roundcube/rcube_message_part.php
+++ program/lib/Roundcube/rcube_message_part.php
@@ -138,4 +138,89 @@ class rcube_message_part
             }
         }
     }
+
+    /**
+     * Normalize and set some part properties from the structure or raw headers
+     *
+     * @param string|rcube_message_header|null $headers Part's raw headers
+     *
+     * @return string Attachment file name
+     */
+    public function normalize($headers = null)
+    {
+        // Some IMAP servers do not support RFC2231, if we have
+        // part headers we'll get attachment name from them, not the BODYSTRUCTURE
+        $rfc2231_params = [];
+        if (!empty($headers) || !empty($this->headers)) {
+            if (is_object($headers)) {
+                $headers = get_object_vars($headers);
+            } else {
+                $headers = !empty($headers) ? rcube_mime::parse_headers($headers) : $this->headers;
+            }
+
+            $ctype = $headers['content-type'] ?? '';
+            $disposition = $headers['content-disposition'] ?? '';
+            $tokens = preg_split('/;[\s\r\n\t]*/', $ctype . ';' . $disposition);
+
+            foreach ($tokens as $token) {
+                // TODO: Use order defined by the parameter name not order of occurrence in the header
+                if (preg_match('/^(name|filename)\*([0-9]*)\*?="*([^"]+)"*/i', $token, $matches)) {
+                    $key = strtolower($matches[1]);
+                    $rfc2231_params[$key] = ($rfc2231_params[$key] ?? '') . $matches[3];
+                }
+            }
+        }
+
+        // Why the order below?
+        // 1. 'name' maybe truncated, but 'filename' not (seen in an email from Thunderbird)
+        // 2. RFC2231 is most-reliable
+
+        if (isset($rfc2231_params['filename'])) {
+            $filename_encoded = $rfc2231_params['filename'];
+        } elseif (isset($rfc2231_params['name'])) {
+            $filename_encoded = $rfc2231_params['name'];
+        } elseif (isset($this->d_parameters['filename*'])) {
+            $filename_encoded = $this->d_parameters['filename*'];
+        } elseif (isset($this->ctype_parameters['name*'])) {
+            $filename_encoded = $this->ctype_parameters['name*'];
+        } elseif (!empty($this->d_parameters['filename'])) {
+            $filename_mime = $this->d_parameters['filename'];
+        } elseif (!empty($this->ctype_parameters['name'])) {
+            $filename_mime = $this->ctype_parameters['name'];
+        } elseif (!empty($this->headers['content-description'])) {
+            $filename_mime = $this->headers['content-description'];
+        }
+
+        // decode filename
+        if (isset($filename_encoded)) {
+            // decode filename according to RFC 2231, Section 4
+            if (preg_match("/^([^']*)'[^']*'(.*)$/", $filename_encoded, $fmatches)) {
+                $filename_charset = $fmatches[1];
+                $filename_encoded = $fmatches[2];
+            }
+
+            $this->filename = rawurldecode($filename_encoded);
+
+            if (!empty($filename_charset)) {
+                $this->filename = rcube_charset::convert($this->filename, $filename_charset);
+            }
+        } elseif (isset($filename_mime)) {
+            // Note: Do not use charset of part/message nor the default charset (#9376)
+            $this->filename = rcube_mime::decode_mime_string($filename_mime, false);
+        }
+
+        // Workaround for invalid Content-Type (#6816)
+        // Some servers for "Content-Type: PDF; name=test.pdf" may return text/plain and ignore name argument
+        if ($this->mimetype == 'text/plain' && !empty($headers['content-type'])) {
+            $tokens = preg_split('/;[\s\r\n\t]*/', $headers['content-type']);
+            $type = rcube_mime::fix_mimetype($tokens[0]);
+
+            if ($type != $this->mimetype) {
+                $this->mimetype = $type;
+                [$this->ctype_primary, $this->ctype_secondary] = explode('/', $this->mimetype);
+            }
+        }
+
+        return $this->filename;
+    }
 }
--- tests/Framework/MessagePart.php
+++ tests/Framework/MessagePart.php
@@ -16,4 +16,60 @@ class Framework_MessagePart extends TestCase
 
         $this->assertInstanceOf('rcube_message_part', $object, 'Class constructor');
     }
+
+    /**
+     * Test for normalize() method
+     */
+    public function test_normalize()
+    {
+        $conf = [
+            'include_bodies' => false,
+            'decode_bodies' => false,
+            'decode_headers' => true,
+        ];
+
+        $mime = new rcube_mime_decode($conf);
+        $message = $mime->decode(file_get_contents(TESTS_DIR . 'src/filename.eml'));
+
+        foreach ($message->parts as $part) {
+            $part->filename = '';
+        }
+
+        // Test some basic cases
+        $this->assertSame('A011.txt', $message->parts[0]->normalize());
+        $this->assertSame('A012.txt', $message->parts[1]->normalize());
+        $this->assertSame('A014.txt', $message->parts[2]->normalize());
+
+        // Test RFC2047 encoding (note: the decoding was done in rcube_mime_decode)
+        $this->assertSame('żółć.png', $message->parts[3]->normalize());
+        $this->assertSame(
+            'very very very very long very very very very long ćććććć very very very long name.txt',
+            $message->parts[5]->normalize()
+        );
+
+        // Test RFC2231 encoding (note: the decoding was done in rcube_mime_decode)
+        $this->assertSame('żółć.png', $message->parts[4]->normalize());
+
+        // Test the decoding in normalize() itself
+        $part = new rcube_message_part();
+
+        $headers = "Content-Type: image/png; charset=UTF-16LE; name=A016.txt\r\n"
+            . "Content-Disposition: attachment;\r\n filename*=UTF-8''%C5%BC%C3%B3%C5%82%C4%87.png\r\n";
+        $this->assertSame('żółć.png', $part->normalize($headers));
+
+        $headers = "Content-Type: text/plain; charset=ISO-8859-1;\r\n"
+            . " name=\"=?UTF-8?Q?very_very_very_very_long_very_very_very_very_long_=C4=87?=\r\n"
+            . " =?UTF-8?Q?=C4=87=C4=87=C4=87=C4=87_very_very_very_long_name=2Etxt?=\r\n"
+            . " =?UTF-8?Q??=\"\r\n"
+            . "Content-Disposition: attachment;\r\n"
+            . " filename*0*=UTF-8''very%20very%20very%20very%20long%20very%20very%20very;\r\n"
+            . " filename*1*=%20very%20long%20%C4%87%C4%87%C4%87%C4%87%C4%87%C4%87%20very;\r\n"
+            . " filename*2*=%20very%20very%20long%20name.txt;\r\n";
+        $this->assertSame(
+            'very very very very long very very very very long ćććććć very very very long name.txt',
+            $part->normalize($headers)
+        );
+
+        // TODO: Test some more corner cases
+    }
 }
