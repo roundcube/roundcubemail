5c0fbde1683f34cbc6ffc91005d0f0ebffb1ed45
Aleksander Machniak <alec@alec.pl>
2024-05-19 11:04:47 +0200

    Fix PHP8 warnings

--- program/lib/Roundcube/rcube.php
+++ program/lib/Roundcube/rcube.php
@@ -833,7 +833,7 @@ class rcube
             $lang = $rcube_language_aliases[$lang];
         }
         // try the first two chars
-        else if (!isset($rcube_languages[$lang])) {
+        else if ($lang && !isset($rcube_languages[$lang])) {
             $short = substr($lang, 0, 2);
 
             // check if we have an alias for the short language code
@@ -846,7 +846,7 @@ class rcube
             }
         }
 
-        if (!isset($rcube_languages[$lang]) || !is_dir(RCUBE_LOCALIZATION_DIR . $lang)) {
+        if (!$lang || !isset($rcube_languages[$lang]) || !is_dir(RCUBE_LOCALIZATION_DIR . $lang)) {
             $lang = 'en_US';
         }
 
