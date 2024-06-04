63c90efa0217668761cfe77bd0c2ed2423d49ead
Pablo Zmdl <pablo@nextcloud.com>
2024-06-04 18:02:48 +0200

    Allow to specify a different IMAP server container image
    To use another image, e.g. to test a different server implementation, or
    because the default image doesn't run on your platform, create a file
    called 'tests/MessageRendering/.env' and assign the variable
    ROUNDCUBE_TEST_IMAP_SERVER_IMAGE in it.

--- tests/MessageRendering/run-tests.sh
+++ tests/MessageRendering/run-tests.sh
@@ -3,11 +3,15 @@
 testsdir=$(realpath $(dirname $0))
 cont_name="roundcubetest-dovecot"
 
+ROUNDCUBE_TEST_IMAP_SERVER_IMAGE="docker.io/dovecot/dovecot:latest"
+
+test -f "$testsdir/.env" && source "$testsdir/.env"
+
 docker run -it --rm -d --name "$cont_name" \
 	-p 143:143 \
 	-v "$testsdir/data/maildir:/srv/mail/test" \
 	-v "$testsdir/dovecot-maildir.conf:/etc/dovecot/conf.d/dovecot-maildir.conf" \
-	docker.io/dovecot/dovecot:latest >/dev/null || exit 1
+	"$ROUNDCUBE_TEST_IMAP_SERVER_IMAGE" >/dev/null || exit 1
 
 "$testsdir/../../vendor/bin/phpunit" -c "$testsdir/phpunit.xml" --fail-on-warning --fail-on-risky
 
