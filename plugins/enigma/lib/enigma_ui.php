ff52b8a160ae4f1090119d7d23ed217c9fc2f4c7
Aleksander Machniak <alec@alec.pl>
2024-06-07 16:07:50 +0200

    Ignore phpstan error

--- plugins/enigma/lib/enigma_ui.php
+++ plugins/enigma/lib/enigma_ui.php
@@ -1311,6 +1311,11 @@ class enigma_ui
     /**
      * Check if the part or its parent exists in the array
      * of decryptions/signatures. Returns found ID.
+     *
+     * @param string $part_id
+     * @param array  $data
+     *
+     * @return string|null
      */
     private function find_part_id($part_id, $data)
     {
@@ -1318,10 +1323,13 @@ class enigma_ui
         $i = 0;
         $count = count($ids);
 
+        // @phpstan-ignore-next-line
         while ($i < $count && strlen($part = implode('.', array_slice($ids, 0, ++$i)))) {
             if (array_key_exists($part, $data)) {
                 return $part;
             }
         }
+
+        return null;
     }
 }
