4062096760f9a9bc0ae548e633a3bf5bfd898318
Pablo Zmdl <pablo@nextcloud.com>
2024-07-19 14:43:28 +0200

    fixup! De-inline event handlers from plugins+actions

--- program/js/app.js
+++ program/js/app.js
@@ -10699,6 +10699,63 @@ function rcube_webmail() {
         }
     };
 
+    this.toggle_html_editor = function (event) {
+        this.toggle_editor({ html: event.target.checked }, null, event);
+    };
+
+    this.toggle_change_subscription = function (elem) {
+        if (elem.checked) {
+            ref.subscribe(elem.value);
+        } else {
+            ref.unsubscribe(elem.value);
+        }
+    };
+
+    this.filter_folder = function (elem) {
+        ref.folder_filter(elem.value);
+    };
+
+    this.reset_value_if_inbox = function (elem) {
+        if ($(elem).val() == 'INBOX') {
+            $(elem).val('');
+        }
+    };
+
+    this.disable_show_images_if_plaintext_preferred = function (elem) {
+        $('#rcmfd_show_images').prop('disabled', !elem.checked).val(0);
+    };
+
+    this.hide_and_show_next = function (elem) {
+        $(elem).hide().next().show();
+    };
+
+    // Allow to call rcmail.message_list.clear() without the extra calls in
+    // rcmail.clear_message_list().
+    this.message_list_clear = function (arg) {
+        ref.message_list.clear(arg);
+    };
+
+    this.onerror_set_placeholder_src = function (event, src) {
+        var elem = event.target;
+        elem.onerror = null;
+        if (!src) {
+            src = this.env.photo_placeholder;
+        }
+        elem.src = src;
+    };
+
+    this.show_sibling_image_attachments = function (elem) {
+        $(elem).parents('p.image-attachment').show();
+    };
+
+    this.reloadForm = function (elem) {
+        this.command('save', 'reload', elem.form);
+    };
+
+    this.toggle_html_signature_editor = function (event) {
+        this.toggle_editor({ id: 'rcmfd_signature', html: event.target.checked }, null, event);
+    };
+
     /**
      * Add eventListeners parsed from element datasets. This allows to avoid
      * inline javascript event handlers (which require a very lax CSP).
