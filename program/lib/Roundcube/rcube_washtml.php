316a0dd455af8f79ec5c22fb20f6e8739ff9d0ae
Aleksander Machniak <alec@alec.pl>
2024-08-16 19:56:51 +0200

    Fix regression where HTML messages were displayed unstyled (#9586)

--- program/lib/Roundcube/rcube_washtml.php
+++ program/lib/Roundcube/rcube_washtml.php
@@ -708,6 +708,12 @@ class rcube_washtml
      */
     public function get_config($prop)
     {
+        $config_props = ['html_elements', 'html_attribs', 'ignore_elements', 'void_elements', 'css_prefix'];
+
+        if (in_array($prop, $config_props)) {
+            return $this->{"_{$prop}"};
+        }
+
         return isset($this->config[$prop]) ? $this->config[$prop] : null;
     }
 
