a4e50e396186f2fc71ffc4a6740d2ad75750433d
Aleksander Machniak <alec@alec.pl>
2024-09-25 11:41:03 +0200

    Managesieve: Fix current script state after initial scripts creation in managesieve_kolab_master mode

--- plugins/managesieve/lib/Roundcube/rcube_sieve_engine.php
+++ plugins/managesieve/lib/Roundcube/rcube_sieve_engine.php
@@ -266,11 +266,11 @@ class rcube_sieve_engine
             // get (first) active script
             if (!empty($this->active)) {
                 $script_name = $this->active[0];
-            } elseif ($list) {
+            } elseif (!empty($list)) {
                 $script_name = $list[0];
             } else {
                 // if script does not exist create one with default content
-                $this->create_default_script();
+                $script_name = $this->create_default_script();
             }
         }
 
