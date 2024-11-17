5101dfb7fbe7c91f3cf73c0439d4fe498eff815a
Michael Voříšek <mvorisek@mvorisek.cz>
2024-11-17 12:42:14 +0100

    manual review

--- plugins/debug_logger/debug_logger.php
+++ plugins/debug_logger/debug_logger.php
@@ -2,6 +2,7 @@
 
 use rcmail;
 use rcube_plugin;
+use runlog;
 
 /**
  * Debug Logger
@@ -71,7 +72,7 @@ class debug_logger extends rcube_plugin
     {
         require_once __DIR__ . '/runlog/runlog.php';
 
-        $this->runlog = new \runlog();
+        $this->runlog = new runlog();
 
         if (!rcmail::get_instance()->config->get('log_dir')) {
             rcmail::get_instance()->config->set('log_dir', INSTALL_PATH . 'logs');
