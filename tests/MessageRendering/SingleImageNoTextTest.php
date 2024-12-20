0d0b619d2576f89dfd167f71ea200196e41db213
Pablo Zmdl <pablo@nextcloud.com>
2024-12-20 16:13:22 +0100

    More MR tests with images

--- tests/MessageRendering/SingleImageNoTextTest.php
+++ tests/MessageRendering/SingleImageNoTextTest.php
@@ -21,4 +21,34 @@ class SingleImageNoTextTest extends MessageRenderingTestCase
         $this->assertCount(1, $attchNames, 'Attachments');
         $this->assertStringStartsWith('Resized_20240427_200026(1).jpeg', $attchNames[0]->textContent);
     }
+
+    /**
+     * Test that an image, that has a Content-ID, which is not accompanied by
+     * an HTML part (and thus is not referred to), is shown as attachment.
+     */
+    public function testShowUnreferredToImagesWithContentId()
+    {
+        $domxpath = $this->runAndGetHtmlOutputDomxpath('yyy@mail.gmail.com');
+
+        $this->assertSame('test', $this->getScrubbedSubject($domxpath));
+
+        $attchNames = $domxpath->query('//span[@class="attachment-name"]');
+        $this->assertCount(1, $attchNames, 'Attachments');
+        $this->assertStringStartsWith('тест.jpg', $attchNames[0]->textContent);
+    }
+
+    /**
+     * Test that an image, that has a Content-ID, but is not referred to in the
+     * accompanying HTML-part, is shown as attachment.
+     */
+    public function testShowUnreferredToImagesWithContentIdInMultipartAlternative()
+    {
+        $domxpath = $this->runAndGetHtmlOutputDomxpath('2ef37d1124655807449f5e405cdd4834b79fb026@example.net');
+
+        $this->assertSame('Multipart/alternative with attached but unreferenced image', $this->getScrubbedSubject($domxpath));
+
+        $attchNames = $domxpath->query('//span[@class="attachment-name"]');
+        $this->assertCount(1, $attchNames, 'Attachments');
+        $this->assertStringStartsWith('Stg Aiki - SB - 22-23 Fév 2025.jpg', $attchNames[0]->textContent);
+    }
 }
