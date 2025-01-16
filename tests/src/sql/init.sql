771b5ced0bbe61f4924fd340c7733de5f28f6b55
Nathan J. Howards <leol20343@gmail.com>
2025-01-16 18:45:25 +0100

    Delete tests directory

--- tests/src/sql/init.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM users;
-INSERT INTO users (user_id, username, mail_host) VALUES(1, 'test@example.com', 'localhost');
--- tests/src/sql/responses.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM responses;
-INSERT INTO responses (user_id, name, data, is_html) VALUES (1, 'response 1', 'test response 1', '0');
-INSERT INTO responses (user_id, name, data, is_html) VALUES (1, 'response 2', '<p><b>test response 2</b></p>', '1');
