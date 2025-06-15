ded28db823c02bd6df526d21a3964c465ea53b09
Aleksander Machniak <alec@alec.pl>
2025-06-15 07:53:47 +0200

    Installer: Move logon warning code into rcmail_install, change colors, fix link

--- program/include/rcmail_output_html.php
+++ program/include/rcmail_output_html.php
@@ -1084,7 +1084,10 @@ class rcmail_output_html extends rcmail_output
                 $prefix = str_repeat('../', count($path) + 1);
             }
 
-            if (!str_starts_with($location, $prefix . 'static.php') && !str_starts_with($location, 'data:')) {
+            if (!str_starts_with($location, $prefix . 'static.php')
+                && !str_starts_with($location, 'data:')
+                && !str_ends_with($location, '.php')
+            ) {
                 $location = $prefix . 'static.php/' . $location;
             }
         }
