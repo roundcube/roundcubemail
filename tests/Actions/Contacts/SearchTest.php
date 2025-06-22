ab7545c196386d1f48e0487bc4c61cc841835bda
PhilW <roundcube@tehinterweb.co.uk>
2025-06-22 08:56:54 +0100

    stub out contact saved search test

--- tests/Actions/Contacts/SearchTest.php
+++ tests/Actions/Contacts/SearchTest.php
@@ -92,4 +92,41 @@ class SearchTest extends ActionTestCase
         $this->assertTrue(strpos($result['exec'], 'this.enable_command("search-create",true);') !== false);
         $this->assertTrue(strpos($result['exec'], 'this.update_group_commands()') !== false);
     }
+
+    /**
+     * Test saved search
+     */
+    public function test_run_saved_search()
+    {
+        $action = new \rcmail_action_contacts_search();
+        $output = $this->initOutput(\rcmail_action::MODE_AJAX, 'contacts', 'search');
+
+        $this->assertTrue($action->checks());
+
+        self::initDB('searches');
+
+        $db = \rcmail::get_instance()->get_dbh();
+        $query = $db->query('SELECT * FROM `searches` WHERE `name` = \'test\'');
+        $result = $db->fetch_assoc($query);
+        $sid = $result['search_id'];
+
+        self::initDB('contacts');
+
+        $_GET = ['_sid' => $sid];
+
+        $this->runAndAssert($action, OutputJsonMock::E_EXIT);
+
+        $result = $output->getOutput();
+
+        $this->assertContains('Content-Type: application/json; charset=UTF-8', $output->headers);
+        $this->assertSame('search', $result['action']);
+        $this->assertSame(1, $result['env']['pagecount']);
+        $this->assertMatchesRegularExpression('/^[0-9a-z]{32}$/', $result['env']['search_request']);
+        $this->assertTrue(strpos($result['exec'], 'this.add_contact_row') !== false);
+        $this->assertTrue(strpos($result['exec'], 'this.set_rowcount("Contacts 1 to 1 of 1");') !== false);
+        $this->assertTrue(strpos($result['exec'], 'this.display_message("1 contacts found.","confirmation",0);') !== false);
+        $this->assertTrue(strpos($result['exec'], 'this.unselect_directory();') === false);
+        $this->assertTrue(strpos($result['exec'], 'this.enable_command("search-create",true);') === false);
+        $this->assertTrue(strpos($result['exec'], 'this.update_group_commands()') !== false);
+    }
 }
