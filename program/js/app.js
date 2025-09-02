d2c721f38f6935e46fde98a6eeee102b94d21b83
Pablo Zmdl <pablo@nextcloud.com>
2025-09-02 17:37:19 +0200

    Allow event handlers to act after a signature was inserted

--- program/js/app.js
+++ program/js/app.js
@@ -5358,6 +5358,7 @@ function rcube_webmail() {
             var lock = this.display_message('', 'loading');
             this.http_get('settings/response-get', { _id: response, _is_html: this.editor.is_html() ? 1 : 0 }, lock);
         }
+        this.triggerEvent('insert_response');
     };
 
     // Updates spellchecker buttons on state change
