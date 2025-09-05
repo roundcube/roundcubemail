e4c962b89242c6f908ca42cf2e712be7531eb6d1
Pablo Zmdl <pablo@nextcloud.com>
2025-09-05 17:03:15 +0200

    Build testrunner images for php v8.4 and 8.5-rc, too

--- .ci/docker-images/build.sh
+++ .ci/docker-images/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 case "$1" in
-	8.1|8.3)
-		phpversion="$1"
-		;;
-	*)
+	''|-*)
 		echo "Error: first and only argument must be the wanted PHP version."
-		echo "Usage: $(basename $0) 8.1|8.3"
+		echo "E.g.: $(basename $0) 8.4"
 		exit 1
 		;;
+	*)
+    	phpversion="$1"
+    	;;
 esac
 
 exec docker build --build-arg "PHPVERSION=$phpversion" -f "$(realpath $(dirname $0)/Dockerfile)" -t "ghcr.io/pabzm/roundcubemail-testrunner:php$phpversion" "$(realpath $(dirname $0)/../../..)"
