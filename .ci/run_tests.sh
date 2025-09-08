75072a3590bd9c100d50f5d644896e52ef5ae0aa
Pablo Zmdl <pablo@nextcloud.com>
2025-09-08 16:38:03 +0200

    Update phpunit a little to decide when to fail on deprecations
    PHPUnit 10.5.47 and later know the flag `--do-not-fail-on-deprecation`, which allows us to make it not exit with code 1
    in case of deprecations on the second run of the script. That second run uses the lowest valid dependencies, which might
    contain deprecations when used with newer versions of PHP, but still are acceptable versions, and should not make our
    tests fail.

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -10,10 +10,11 @@ composer require $COMPOSER_ARGS "kolab/net_ldap3:~1.1.4" --no-update
 composer update $COMPOSER_ARGS --prefer-dist --no-interaction --no-progress
 
 # Execute tests.
-vendor/bin/phpunit -c tests/phpunit.xml --fail-on-warning --fail-on-risky
+vendor/bin/phpunit -c tests/phpunit.xml --fail-on-warning --fail-on-risky --display-deprecations
 
 # Downgrade dependencies to the lowest versions.
 composer update $COMPOSER_ARGS --prefer-dist --prefer-stable --prefer-lowest --no-interaction --no-progress --optimize-autoloader
 
-# Execute tests again.
-vendor/bin/phpunit -c tests/phpunit.xml --fail-on-warning --fail-on-risky
+# Execute tests again. We do not want this run to fail due to deprecations though, because those can only occur from
+# (older) dependencies, which we migh still consider good enough.
+vendor/bin/phpunit -c tests/phpunit.xml --fail-on-warning --fail-on-risky --display-deprecations --do-not-fail-on-deprecation
