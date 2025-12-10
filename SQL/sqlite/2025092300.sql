a3611fa6a413ceb292e365813b029af41d14df9a
Pablo Zmdl <pablo@nextcloud.com>
2025-12-10 12:07:22 +0100

    Fix renaming SQL index in sqlite migration

--- SQL/sqlite/2025092300.sql
+++ SQL/sqlite/2025092300.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `session` RENAME COLUMN `changed` TO `expires_at`;
-ALTER TABLE `session` RENAME INDEX `ix_session_changed` TO `ix_session_expires_at`;
+DROP INDEX ix_session_changed;
+CREATE INDEX ix_session_expires_at ON session ("expires_at");
 UPDATE session SET expires_at = DATETIME(expires_at, '+10 minutes');
