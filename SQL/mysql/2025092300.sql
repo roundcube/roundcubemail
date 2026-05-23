d5d83ecf2e8321ba1e07eed9da6869b8703b63c4
Aleksander Machniak <alec@alec.pl>
2026-05-23 08:33:55 +0200

    Fix MySQL upgrade on MySQL < 8.0 and MariaDB < 10.5.3 (#10188)

--- SQL/mysql/2025092300.sql
+++ SQL/mysql/2025092300.sql
@@ -1,3 +1,4 @@
-ALTER TABLE `session` RENAME COLUMN `changed` TO `expires_at`;
-ALTER TABLE `session` RENAME INDEX `changed_index` TO `expires_at_index`;
+ALTER TABLE `session` CHANGE COLUMN `changed` `expires_at` datetime NOT NULL DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE `session` DROP INDEX `changed_index`;
+ALTER TABLE `session` ADD INDEX `expires_at_index` (`expires_at`);
 UPDATE session SET expires_at = ADDTIME(expires_at, '00:10:00');
