60052688ee65305835804a0698304119e2f6955f
Michael Voříšek <mvorisek@mvorisek.cz>
2026-06-23 15:48:10 +0200

    Improve PHP CS Fixer config/speed (#10240)
    * Revert "Fix php-cs-fixer"
    
    This reverts commit d91a4189429ec088202d38b04bee8ec9d13215a3.
    
    * fix properly

--- .php-cs-fixer-fully_qualified_strict_types.php
+++ .php-cs-fixer-fully_qualified_strict_types.php
@@ -228,7 +228,7 @@ class Foo extends \Other\BaseClass implements \Other\Interface1, \Other\Interfac
      */
     public function getPriority(): int
     {
-        return 6;
+        return 7;
     }
 
     public function isCandidate(Tokens $tokens): bool
